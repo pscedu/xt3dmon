a4e5a14ae30ced3f8abe79eddf6e9af991ccdea4
yanovich <>
2005-03-25 22:37:17 +0000

    more initial work

--- main.c
+++ main.c
@@ -15,41 +15,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define _PATH_JLIST1 "/home/torque/nids_list_login1"
-#define _PATH_JLIST2 "/home/torque/nids_list_login2"
+#define _PATH_JOBMAP	"/usr/users/torque/nids_list_login%d"
+#define _PATH_PHYSMAP	"/opt/tmp-harness/default/ssconfig/sys%d/nodelist"
 
-#define WIN_WIDTH		500
-#define WIN_HEIGHT		500
+#define WIN_WIDTH	500
+#define WIN_HEIGHT	500
 
-#define ST_FREE			1
-#define ST_DOWN			2
-#define ST_USED			3
+#define ST_FREE		1
+#define ST_DOWN		2
+#define ST_USED		3
 
-#define NROWS			2
-#define NCABS			10
-#define NCAGES			3
-#define NMODS			8
-#define NNODES			4
+#define NROWS		2
+#define NCABS		10
+#define NCAGES		3
+#define NMODS		8
+#define NNODES		4
 
 typedef int nodeid_t;
 typedef int jobid_t;
 
 struct job {
 	jobid_t			j_id;
-	SLIST_HEAD(node)	j_nodes;
-	SLIST_ENTRY(job)	j_next;
+	LIST_HEAD(, node)	j_nodes;
+	LIST_ENTRY(job)		j_next;
 };
 
 struct node {
-	nodeid_t		n_id;
-	jobid_t			n_jobid;
-	int			n_status;
+	nodeid_t	n_id;
+	jobid_t		n_jobid;
+	int		n_status;
 };
 
-SLIST_HEAD(job)			jobs;
-struct node			nodes[NROWS][NCABS][NCAGES][NMODS][NNODES];
-GLfloat 			angle;
-	
+void			orient(float);
+void			move(int);
+
+int			logids[] = {0, 8};
+LIST_HEAD(, job)	jobs;
+struct node		nodes[NROWS][NCABS][NCAGES][NMODS][NNODES];
+GLfloat 		angle;
+float			x, y, z, lx, ly, lz;
+GLint			cluster_dl;
+
+#define			NLOGIDS (sizeof(logids) / sizeof(logids[0]))
+
 void
 reshape(int width, int height)
 {
@@ -95,10 +103,12 @@ sp_key(int key, int x, int y)
 void
 orient(float angle)
 {
+	float lx, ly;
+
 	lx = sin(angle);
 	ly = -cos(angle);
 	glLoadIdentity();
-	glutLookAt(x, y, z,
+	gluLookAt(x, y, z,
 	    x + lx, y + ly, z + lz,
 	    0.0f, 1.0f, 0.0f);
 }
@@ -109,7 +119,7 @@ move(int dir)
 	x += dir * lx * 0.1;
 	y += dir * lz * 0.1;
 	glLoadIdentity();
-	glutLookAt(x, y, z,
+	gluLookAt(x, y, z,
 	    x + lx, y + ly, z + lz,
 	    0.0f, 1.0f, 0.0f);
 }
@@ -120,45 +130,50 @@ draw(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	glRotatef(60.0, 0.0, 1.0, 0.0);
-	glCallList(BAR);
-	glTranslatef(0.0, 30.0, 0.0);
+
 	glRotatef(angle, 1.0, 0.0, 0.0);
 	glTranslatef(0.0, -30.0, 0.0);
-	glCallList(SWING);
+	glCallList(cluster_dl);
+
 	glFlush();
 	glutSwapBuffers();
-
 }
 
-void
-make_cluster(void)
+/*
+ *             z
+ *            /
+ *           /
+ *          /
+ * (0,0,0) +-------------------------------------------------------- x
+ *         |                 (x,y,z+DEPTH)   (x+WIDTH,y,z+DEPTH)
+ *         |                         +----------+
+ *         |                        /   Top    /|
+ *         |                       / |        / |
+ *         |              (x,y,z) +----------+  | (x+WIDTH,y,z)
+ *         |                      |   (Back) |  |
+ *         |                      |  |       |  | Right
+ *         |                      |          |  |
+ *         |                 Left |  | Front |  |
+ *         |                      |          |  |
+ *         | (x,y+HEIGHT,z+DEPTH) |  +  -  - |- + (x+WIDTH,y+HEIGHT,z+DEPTH)
+ *         |                      | /        | /
+ *         |                      | (Bottom) |/
+ *         |                      +----------+
+ *         |        (x,y+HEIGHT,z) (x+WIDTH,y+HEIGHT,z)
+ *         |
+ *         y
+ */
+__inline void
+draw_node(int x, int y, int z, int length, int width, int depth)
 {
-	int r, cb, cg, m, n, total;
-
-	/* Assign node IDs. */
-	for (r = 0; r < NROWS; r++)
-		for (cb = 0; cb < NCABS; cb++)
-			for (cg = 0; cg < NCAGES; cg++)
-				for (m = 0; m < NMODS; m++)
-					for (n = 0; n < NNODES; n++, total++)
-						nodes[r][cb][cg][m][n].n_id =
-						    total;
-}
-
-void
-make_bar(void)
-{
-	glNewList(BAR, GL_COMPILE);
-	glBegin(GL_QUADS);
 	glColor3f(1.0, 1.0, 0.0);
 
-	// Bottom
+	/* Bottom */
 	glVertex3d(-50, 30, -1);
 	glVertex3d(50, 30, -1);
 	glVertex3d(50, 30, 1);
 	glVertex3d(-50, 30, 1);
-	// Top
+	/* Top */
 	glVertex3d(-50, 33, -1);
 	glVertex3d(50, 33, -1);
 	glVertex3d(50, 33, 1);
@@ -166,12 +181,12 @@ make_bar(void)
 
 	glColor3f(1.0, 0.65, 0.0);
 
-	// Front
+	/* Front */
 	glVertex3d(-50, 33, 1);
 	glVertex3d(-50, 30, 1);
 	glVertex3d(50, 30, 1);
 	glVertex3d(50, 33, 1);
-	// Back
+	/* Back */
 	glVertex3d(50, 33, -1);
 	glVertex3d(50, 30, -1);
 	glVertex3d(-50, 30, -1);
@@ -179,94 +194,67 @@ make_bar(void)
 
 	glColor3f(1.0, 0.0, 0.0);
 
-	// Left
+	/* Left */
 	glVertex3d(-50, 33, -1);
 	glVertex3d(-50, 30, -1);
 	glVertex3d(-50, 30, 1);
 	glVertex3d(-50, 33, 1);
-	// Right
+	/* Right */
 	glVertex3d(50, 33, 1);
 	glVertex3d(50, 30, 1);
 	glVertex3d(50, 30, -1);
 	glVertex3d(50, 33, -1);
-	glEnd();
-
-	glEndList();
-}
-
-void
-make_swing(void)
-{
-	glNewList(SWING, GL_COMPILE);
-
-	glColor3f(1.0, 0.0, 1.0);
-	glLineWidth(5.0);
-
-	// Draw the ropes
-	glBegin(GL_LINES);
-	glVertex3d(-20, 30, 0);
-	glVertex3d(-20, -30, 0);
-	glVertex3d(20, 30, 0);
-	glVertex3d(20, -30, 0);
-	glEnd();
-
-	glColor3f(0.0, 1.0, 0.0);
-
-	// Draw the sides of the seat
-	glBegin(GL_QUAD_STRIP);
-	glVertex3d(20, -30, -10);
-	glVertex3d(20, -33, -10);
-	glVertex3d(-20, -30, -10);
-	glVertex3d(-20, -33, -10);
-	glVertex3d(-20, -30, 10);
-	glVertex3d(-20, -33, 10);
-	glVertex3d(20, -30, 10);
-	glVertex3d(20, -33, 10);
-	glVertex3d(20, -30, -10);
-	glVertex3d(20, -33, -10);
-	glEnd();
-
-	glColor3f(0.0, 0.0, 1.0);
-
-	// Draw the top and bottom of the seat
-	glBegin(GL_QUADS);
-	glVertex3d(-20, -33, -10);
-	glVertex3d(20, -33, -10);
-	glVertex3d(20, -33, 10);
-	glVertex3d(-20, -33, 10);
-
-	glVertex3d(-20, -30, -10);
-	glVertex3d(20, -30, -10);
-	glVertex3d(20, -30, 10);
-	glVertex3d(-20, -30, 10);
-	glEnd();
-
-	glEndList();
-}
-
-void
-load_cluster(void)
-{
-	int i, j, k, l, m;
-
-	for (r = 0; r < NROWS; r++) {
-		for (cb = 0; cb < NCABS; cb++) {
-			for (cg = 0; cg < NCAGES; cg++) {
-				for (:)
-			}
-		}
-	}
 }
 
 void
 make_cluster(void)
 {
-	load_cluster();
+	int r, cb, cg, m, n;
+	int rowdepth, rowspace;
+	int cabwidth, cabspace;
+	int cageheight, cagespace;
+	int modwidth, modspace;
+	int nodewidth, nodeheight, nodedepth;
+
+	rowspace = 50;
+	cabspace = 25;
+	cagespace = 10;
+	modspace = 5;
+
+	nodewidth = nodeheight = nodedepth = 10;
+	modwidth = nodewidth;
+	cageheight = nodeheight * 2;
+	cabwidth = modwidth * NMODS;
+	rowdepth = nodedepth * 2;
+
+	cluster_dl = glGenLists(1);
+	glNewList(cluster_dl, GL_COMPILE);
+	glBegin(GL_QUADS);
+	for (r = 0; r < NROWS; r++, z += rowdepth + rowspace) {
+		for (cb = 0; cb < NCABS; cb++, x += cabwidth + cabspace) {
+			for (cg = 0; cg < NCAGES; cg++, y += cageheight + cagespace) {
+				for (m = 0; m < NMODS; m++, x += modwidth + modspace) {
+					for (n = 0; n < NNODES; n++) {
+						draw_node(x,
+						    y + nodeheight * (n % (NNODES/2)),
+						    z + nodedepth * (n / (NNODES/2)),
+						    nodewidth,
+						    nodeheight,
+						    nodedepth);
+					}
+				}
+				x -= (modwidth + modspace) * NMODS;
+			}
+			y -= (cageheight + cagespace) * NCAGES;
+		}
+		x -= (cabwidth + cabspace) * NCABS;
+	}
+	glEnd();
+	glEndList();
 }
 
-
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -280,6 +268,7 @@ main(int argc, char **argv)
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable(GL_DEPTH_TEST);
 
+	parse_physmap();
 	make_cluster();
 
 	angle = -39.0;
