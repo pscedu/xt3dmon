6ac3d78118e030db9b28e9211cb91c7cd0820900
rbudden <>
2005-05-13 16:23:41 +0000

    fix texture load to support alpha channel blending

--- png.c
+++ png.c
@@ -191,7 +191,12 @@ void LoadTexture(void *data, int id)
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_NEAREST);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_NEAREST);
    
-   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width,
+   //glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, width,
+   //glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width,
+   //glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE_ALPHA, width,
+
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_INTENSITY, width,
+   //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width,
                height, 0, GL_RGBA,
                GL_UNSIGNED_BYTE, data);
 
