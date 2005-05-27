a452dd776f6e44010e5c66b0c61b494490cc7b46
yanovich <>
2005-05-27 13:36:35 +0000

    space; use PATH_MAX instead of homegrown constant; use err where appropriate; add some needed includes

--- capture.c
+++ capture.c
@@ -1,19 +1,21 @@
 /* $Id$ */
 
-#include<stdio.h>
-#include<string.h>
-#include<png.h>
-#include<err.h>
+#include <sys/param.h>
 
-#if defined(__APPLE_CC__)
-#include<OpenGL/gl.h>
-#include<GLUT/glut.h>
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <png.h>
+
+#ifdef __APPLE_CC__
+# include <OpenGL/gl.h>
+# include <GLUT/glut.h>
 #else
-#include <GL/gl.h>
-#include <GL/freeglut.h>
+# include <GL/gl.h>
+# include <GL/freeglut.h>
 #endif
-
-#define MAX_FILENAME 120000
 
 /* Take a screenshot from the current framebuffer (PPM) */
 void screenshot_ppm(char *file, int x, int y, int w, int h)
@@ -22,17 +24,13 @@ void screenshot_ppm(char *file, int x, int y, int w, int h)
 	unsigned char *buf;
 	FILE *fp;
 
-	fp = fopen(file,"wb");
-
-	if(!fp)
-	{
-		printf("fopen err\n");
-		exit(1);
-	}
+	if ((fp = fopen(file,"wb")) == NULL)
+		err(1, "%s", file);
 
 	/* Size = num pixels * 3 bytes per pixel (RGB) */
 	size = w*h*3;
-	buf = malloc(size * sizeof(unsigned char));
+	if ((buf = malloc(size * sizeof(unsigned char))) == NULL)
+		err(1, "malloc");
 
 	/* Read data from the framebuffer */
 	glReadPixels(x, y, w, h, GL_RGB, GL_UNSIGNED_BYTE, buf);
@@ -53,15 +51,14 @@ void screenshot_png(char *file, int x, int y, int w, int h)
 	png_structp png;
 	png_infop info;
 	png_bytepp rows;
-	
-	fp = fopen(file,"wb");
 
-	if(!fp)
-		err(1, "fopen failed");
+	if ((fp = fopen(file,"wb")) == NULL)
+		err(1, "%s", file);
 
 	/* Size = num pixels * 4 bytes per pixel (RGBA) */
 	size = w*h*4;
-	buf = malloc(size * sizeof(unsigned char));
+	if ((buf = malloc(size * sizeof(unsigned char))) == NULL)
+		err(1, "malloc");
 
 	/* Read data from the framebuffer */
 	glReadPixels(x, y, w, h, GL_RGBA, GL_UNSIGNED_BYTE, buf);
@@ -70,14 +67,14 @@ void screenshot_png(char *file, int x, int y, int w, int h)
 	png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	if(!png)
 		errx(1, "create_write_struct failed");
-	
+
 	info = png_create_info_struct(png);
 	if(!info)
 	{
 		png_destroy_write_struct(&png, (png_infopp)(NULL));
 		errx(1, "create_info_struct failed");
 	}
-	
+
 	if(setjmp(png_jmpbuf(png)))
 	{
 		png_destroy_write_struct(&png, (png_infopp)(NULL));
@@ -93,14 +90,14 @@ void screenshot_png(char *file, int x, int y, int w, int h)
 	png_set_IHDR(png, info, w, h, 8, PNG_COLOR_TYPE_RGB_ALPHA,
 		PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
 		PNG_FILTER_TYPE_DEFAULT);
-		
+
 	png_write_info(png, info);
-	
+
 	/* Pack single byte pixels */
 	png_set_packing(png);
 
 	if((rows = malloc(sizeof(png_bytep)*h)) == NULL)
-		errx(1, "malloc fail");
+		err(1, "malloc");
 
 	/*
 	** Parse into rows [RGBA (4 pix * width)]
@@ -114,7 +111,7 @@ void screenshot_png(char *file, int x, int y, int w, int h)
 	png_write_image(png, rows);
 	png_write_end(png, info);
 	png_destroy_write_struct(&png, &info);
-	
+
 	free(rows);
 	free(buf);
 
@@ -132,17 +129,17 @@ void invert()
 /* Dump the entire framebuffer */
 void capture_fb(void)
 {
-	char file[MAX_FILENAME];
+	char file[PATH_MAX];
 	GLint vp[4];
 	static long f = 0;
 
-	memset(file, (int)'\0', MAX_FILENAME);
-	snprintf(file, MAX_FILENAME, "ppm/%ld.png", f++);
+	memset(file, 0, sizeof(file));
+	snprintf(file, sizeof(file), "ppm/%ld.png", f++);
 
 	glMatrixMode(GL_PROJECTION);
 	glGetIntegerv(GL_VIEWPORT, vp);
-	
+
 	screenshot_png(file, vp[0], vp[1], vp[2], vp[3]);
-	
+
 	glMatrixMode(GL_MODELVIEW);
 }
