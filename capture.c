aee25e8c0e4fee1e5db29bdde771015f57e91997
rbudden <>
2005-05-31 20:34:15 +0000

    remove redundant code

--- capture.c
+++ capture.c
@@ -52,16 +52,8 @@ void screenshot_ppm(char *file, int x, int y, int w, int h)
 /* Take a screenshot from the current framebuffer (PNG) */
 void screenshot_png(char *file, int x, int y, int w, int h)
 {
-	FILE *fp;
-	int i;
 	long size;
 	unsigned char *buf;
-	png_structp png;
-	png_infop info;
-	png_bytepp rows;
-
-	if ((fp = fopen(file,"wb")) == NULL)
-		err(1, "%s", file);
 
 	/* Size = num pixels * 4 bytes per pixel (RGBA) */
 	size = w*h*4;
@@ -71,59 +63,10 @@ void screenshot_png(char *file, int x, int y, int w, int h)
 	/* Read data from the framebuffer */
 	glReadPixels(x, y, w, h, GL_RGBA, GL_UNSIGNED_BYTE, buf);
 
-	/* Setup PNG file structure */
-	png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if(!png)
-		errx(1, "create_write_struct failed");
+	/* Write data to buf */
+	data2png(file, buf, w, h);
 
-	info = png_create_info_struct(png);
-	if(!info)
-	{
-		png_destroy_write_struct(&png, (png_infopp)(NULL));
-		errx(1, "create_info_struct failed");
-	}
-
-	if(setjmp(png_jmpbuf(png)))
-	{
-		png_destroy_write_struct(&png, (png_infopp)(NULL));
-		errx(1, "terminating");
-	}
-
-	/* We want io with no zlib compression
-	** (highest quality, & fast) */
-	png_init_io(png, fp);
-	png_set_compression_level(png, Z_NO_COMPRESSION);
-
-	/* Set the header/info */
-	png_set_IHDR(png, info, w, h, 8, PNG_COLOR_TYPE_RGB_ALPHA,
-		PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
-		PNG_FILTER_TYPE_DEFAULT);
-
-	png_write_info(png, info);
-
-	/* Pack single byte pixels */
-	png_set_packing(png);
-
-	if((rows = malloc(sizeof(png_bytep)*h)) == NULL)
-		err(1, "malloc");
-
-	/*
-	** Parse into rows [RGBA (4 pix * width)]
-	** Note: this takes into account vertical flip
-	** use rows[h] then call invert() does the same.
-	*/
-	for(i = 0; i < h; i++)
-		rows[h-i-1] = buf+(i*w*4);
-
-	/* Actaully write the data */
-	png_write_image(png, rows);
-	png_write_end(png, info);
-	png_destroy_write_struct(&png, &info);
-
-	free(rows);
 	free(buf);
-
-	fclose(fp);
 }
 
 /*
@@ -140,7 +83,6 @@ void data2png(char *file, unsigned char *buf, long w, long h)
 	FILE *fp;
 	int i;
 	long size;
-//	unsigned char *buf;
 	png_structp png;
 	png_infop info;
 	png_bytepp rows;
@@ -190,7 +132,6 @@ void data2png(char *file, unsigned char *buf, long w, long h)
 	** Note: this takes into account vertical flip
 	** use rows[h] then call invert() does the same.
 	*/
-	printf("buf - %p\n", buf);
 	for(i = 0; i < h; i++)
 		rows[h-i-1] = buf+(i*w*4);
 
@@ -260,9 +201,9 @@ void capture_fb(void)
 	glMatrixMode(GL_PROJECTION);
 	glGetIntegerv(GL_VIEWPORT, vp);
 	
-//	screenshot_png(file, vp[0], vp[1], vp[2], vp[3]);
+	screenshot_png(file, vp[0], vp[1], vp[2], vp[3]);
 //	screenshot_ppm(file, vp[0], vp[1], vp[2], vp[3]);
-	screenshot_raw(vp[0], vp[1], vp[2], vp[3]);
+//	screenshot_raw(vp[0], vp[1], vp[2], vp[3]);
 	
 	glMatrixMode(GL_MODELVIEW);
 }
