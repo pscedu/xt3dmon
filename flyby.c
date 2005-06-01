03b7beacbf8b0bfe49bbfc40c037416a42eef793
yanovich <>
2005-06-01 20:20:34 +0000

    more initial code

--- flyby.c
+++ flyby.c
@@ -22,10 +22,10 @@
  */
 
 const struct state flybypath[] = {
- /*      x        y        z      lx      ly      lz  options                          panels       ajob  aoth  afmt     tween_mode   nframes */
- {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | /* OP_TWEEN | */ OP_GROUND, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
- { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | /* OP_TWEEN | */ OP_GROUND, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
- { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | /* OP_TWEEN | */ OP_GROUND, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
- { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | /* OP_TWEEN | */ OP_GROUND, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
- {   0.00f,   0.00f,   0.00f,  0.00f,  0.00f,  0.00f, 0,                               0,           0.0f, 0.0f, 0,       0,           0   }
+ /*      x        y        z      lx      ly      lz  options                            panels       ajob  aoth  afmt     tween_mode   nframes */
+ {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | OP_GROUND | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
+ { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | OP_GROUND | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
+ { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | OP_GROUND | OP_DISPLAY, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
+ { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | OP_GROUND | OP_DISPLAY, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
+ {   0.00f,   0.00f,   0.00f,  0.00f,  0.00f,  0.00f, 0,                                 0,           0.0f, 0.0f, 0,       0,           0   }
 };
