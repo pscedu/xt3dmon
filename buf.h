59927e272ddcd7df872f4628d075e7ea25a83616
rbudden <>
2005-06-07 18:06:41 +0000

    semicolons

--- buf.h
+++ buf.h
@@ -15,13 +15,13 @@ struct buf {
 	char	*buf_buf;
 };
 
-void	 buf_init(struct buf *buf)
-void	 buf_realloc(struct buf *buf)
-void	 buf_append(struct buf *buf, char ch)
-char	*buf_get(struct buf *buf)
-void	 buf_set(struct buf *buf, char *s)
-void	 buf_free(struct buf *buf)
-void	 buf_reset(struct buf *buf)
-void	 buf_chop(struct buf *buf)
-int	 buf_len(struct buf *buf)
-void	 buf_cat(struct buf *ba, struct buf *bb)
+void	 buf_init(struct buf *buf);
+void	 buf_realloc(struct buf *buf);
+void	 buf_append(struct buf *buf, char ch);
+char	*buf_get(struct buf *buf);
+void	 buf_set(struct buf *buf, char *s);
+void	 buf_free(struct buf *buf);
+void	 buf_reset(struct buf *buf);
+void	 buf_chop(struct buf *buf);
+int	 buf_len(struct buf *buf);
+void	 buf_cat(struct buf *ba, struct buf *bb);
