0aa6e454d189f2fa763bbe114e46bf412de68d43
rbudden <>
2005-06-07 17:49:44 +0000

    intial flyby capture mode code

--- flyby.c
+++ flyby.c
@@ -1,6 +1,8 @@
 /* $Id$ */
 
 #include "mon.h"
+#include<stdio.h>
+#include<err.h>
 
 /*
  * struct state {
@@ -23,9 +25,83 @@
 
 const struct state flybypath[] = {
  /*      x        y        z      lx      ly      lz  options                            panels       ajob  aoth  afmt     tween_mode   nframes */
- {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | OP_GROUND | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
- { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | OP_GROUND | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
- { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | OP_GROUND | OP_DISPLAY, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
- { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | OP_GROUND | OP_DISPLAY, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
+#if 0
+ {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | OP_CAPTURE | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
+ { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | OP_CAPTURE | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
+ { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | OP_CAPTURE | OP_DISPLAY, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
+ { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | OP_CAPTURE | OP_DISPLAY, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
  {   0.00f,   0.00f,   0.00f,  0.00f,  0.00f,  0.00f, 0,                                 0,           0.0f, 0.0f, 0,       0,           0   }
 };
+ #endif
+
+#if 0
+ {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | OP_CAPTURE | OP_TEX | OP_BLEND, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
+ { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | OP_CAPTURE | OP_TEX | OP_BLEND, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
+ { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | OP_CAPTURE | OP_TEX | OP_BLEND, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
+ { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | OP_CAPTURE | OP_TEX | OP_BLEND, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
+ {   0.00f,   0.00f,   0.00f,  0.00f,  0.00f,  0.00f, 0,                                 0,           0.0f, 0.0f, 0,       0,           0   }
+};
+#endif
+
+ {  50.00f,  20.00f,  10.00f, -0.40f,  0.10f,  0.40f, OP_WIRES | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 200 },
+ { -10.00f,   8.00f,  12.00f,  0.60f, -0.15f,  0.50f, OP_WIRES | OP_DISPLAY, 0,           1.0f, 1.0f, GL_RGBA, TM_RADIUS,   200 },
+ { 238.29f,  26.90f, -28.59f, -0.68f, -0.15f,  0.38f, OP_WIRES | OP_DISPLAY, PANEL_FPS,   1.0f, 1.0f, GL_RGBA, TM_RADIUS,   500 },
+ { 136.09f,   6.21f,   8.50f, -0.78f, -0.15f,  0.02f, OP_WIRES | OP_DISPLAY, PANEL_NINFO, 1.0f, 1.0f, GL_RGBA, TM_STRAIGHT, 100 },
+ {   0.00f,   0.00f,   0.00f,  0.00f,  0.00f,  0.00f, 0,                                 0,           0.0f, 0.0f, 0,       0,           0   }
+};
+
+
+#define FLYBY_FILE "flyby.data"
+static FILE *flyby_fp;
+
+void begin_flyby_build()
+{
+	if((flyby_fp = fopen(FLYBY_FILE, "ab")) == NULL)
+		err(1, "flyby data file err");
+}
+
+void end_flyby_build()
+{
+	fclose(flyby_fp);
+}
+
+/* Write current data for flyby */
+void write_flyby()
+{
+	size_t n;
+	
+	if(!fwrite(&st, sizeof(struct state), 1, flyby_fp))
+		err(1, "flyby data write err");
+}
+
+void begin_flyby()
+{
+	if((flyby_fp = fopen(FLYBY_FILE, "rb")) == NULL)
+		err(1, "flyby data file err");
+}
+
+/* Read a set of flyby data */
+void read_flyby()
+{
+	size_t n;
+
+	if(!fread(&st, sizeof(struct state), 1, flyby_fp)) {
+
+		/* end of flyby */
+		if(feof(flyby_fp) != 0)
+			active_flyby = 0;	
+		else {
+			err(1, "flyby data read err");
+			active_flyby = 0;	
+		}
+	}
+}
+
+void end_flyby()
+{
+	fclose(flyby_fp);
+}
+
+
+
+
