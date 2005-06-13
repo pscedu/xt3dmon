dfd15210b416dea99638a781e6822daeb67dc43c
yanovich <>
2005-06-13 17:59:23 +0000

    cleanup

--- flyby.c
+++ flyby.c
@@ -4,85 +4,80 @@
 #include<stdio.h>
 #include<err.h>
 
-#define FLYBY_FILE "flyby.data"
 static FILE *flyby_fp;
 struct flyby fb;
 
-/* Open the flyby data file appropriately */
-void begin_flyby(char m)
+/* Open the flyby data file appropriately. */
+void
+begin_flyby(char m)
 {
 	if(flyby_fp != NULL)
 		return;
 
-	if(m == 'r')
-	{
-		if((flyby_fp = fopen(FLYBY_FILE, "rb")) == NULL)
-			err(1, "%s", FLYBY_FILE);
+	if (m == 'r') {
+		if ((flyby_fp = fopen(_PATH_FLYBY, "rb")) == NULL)
+			err(1, "%s", _PATH_FLYBY);
 	} else if (m == 'w') {
-		if((flyby_fp = fopen(FLYBY_FILE, "ab")) == NULL)
-			err(1, "%s", FLYBY_FILE);
+		if ((flyby_fp = fopen(_PATH_FLYBY, "ab")) == NULL)
+			err(1, "%s", _PATH_FLYBY);
 	}
 }
 
-/* Write current data for flyby */
-void write_flyby(void)
+/* Write current data for flyby. */
+void
+write_flyby(void)
 {
 	int tnid = -1, tnlid;
 
-	/* Save the node id instead of ptr */
-	if(selnode != NULL) {
-
-		/* Save node and items we need */
+	/* Save the node ID. */
+	if (selnode != NULL) {
 		tnid = selnode->n_nid;
 		tnlid = selnode->n_logid;
 
-		/* Switch and set nid */
+		/* Switch and set nid. */
 		fb.fb_nid = tnid;
 		fb.fb_nlid = tnlid;
-
 	} else {
 		fb.fb_nid = -1;
 //		fb.fb_nlid = -1;
 	}
 
-	if(!fwrite(&st, sizeof(struct state), 1, flyby_fp))
-		err(1, "flyby data write err");
+	if (fwrite(&st, sizeof(struct state), 1, flyby_fp) != 1)
+		err(1, "fwrite");
 	
 	/* Set node back */
-	if(tnid != -1)
-		selnode = invmap[tnlid][tnid];
-	else
+	if (tnid == -1)
 		selnode = NULL;
+	else
+		selnode = invmap[tnlid][tnid];
 
 }
 
-/* Read a set of flyby data */
-void read_flyby(void)
+/* Read a set of flyby data. */
+void
+read_flyby(void)
 {
-	if(!fread(&st, sizeof(struct state), 1, flyby_fp)) {
-
-		/* end of flyby */
-		if(feof(flyby_fp) != 0) {
+	if (fread(&st, sizeof(struct state), 1, flyby_fp) != 1) {
+		/* End of flyby. */
+		if (feof(flyby_fp)) {
 			active_flyby = 0;
 			glutKeyboardFunc(keyh_default);
 			glutSpecialFunc(spkeyh_default);
-		}
-		else
-			err(1, "flyby data read err");
+		} else
+			err(1, "fread");
 	}
 
 	if ((st.st_ro & OP_TWEEN) == 0)
 		adjcam();
 
-	/* Restore selected node */
-	if(fb.fb_nid != -1) {
-
+	/* Restore selected node. */
+	if (fb.fb_nid != -1) {
 		int tnid, tnlid;
 		tnid = fb.fb_nid;
 		tnlid = fb.fb_nlid;
 		selnode = invmap[tnlid][tnid];
 
-		/* Force recompile */
+		/* Force recompile. */
 		st.st_ro |= RO_COMPILE;
 printf(" Restore Selected Node: COMPILE\n");
 	} else {
@@ -93,24 +88,26 @@ printf(" No node selected\n");
 	restore_state(1);
 }
 
-void end_flyby(void)
+void
+end_flyby(void)
 {
-	if(flyby_fp != NULL) {
+	if (flyby_fp != NULL) {
 		fclose(flyby_fp);
 		flyby_fp = NULL;
 	}
 }
 
-void update_flyby(void)
+void
+update_flyby(void)
 {
-	/* record user commands */
-	if(build_flyby) {
+	/* Record user commands. */
+	if (build_flyby) {
 		write_flyby();
 		st.st_ro = 0;
 	}
 
-	/* replay */
-	if(active_flyby)
+	/* Replay. */
+	if (active_flyby)
 		read_flyby();
 }
 
