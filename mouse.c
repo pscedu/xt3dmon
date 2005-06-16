89d42d9691f925b19ec118ba1d1af17725495012
yanovich <>
2005-06-16 14:19:05 +0000

    initial checkin

---
+++ mouse.c
@@ -0,0 +1,55 @@
+/* $Id$ */
+
+#include "cdefs.h"
+#include "mon.h"
+
+int	 spkey, lastu, lastv;
+
+void
+mouseh(__unused int button, __unused int state, int u, int v)
+{
+	if (active_flyby)
+		return;
+	spkey = glutGetModifiers();
+	if (spkey == 0 && selnode != NULL &&
+	    button == GLUT_LEFT_BUTTON &&
+	    state == GLUT_DOWN)
+		panel_toggle(PANEL_NINFO);
+	lastu = u;
+	lastv = v;
+}
+
+void
+m_activeh_default(int u, int v)
+{
+	int du = u - lastu, dv = v - lastv;
+
+	if (abs(du) + abs(dv) <= 1)
+		return;
+	lastu = u;
+	lastv = v;
+	rotate_cam(du, dv);
+}
+
+void
+m_passiveh_default(int u, int v)
+{
+	lastu = u;
+	lastv = v;
+
+	detect_node(u, v);
+}
+
+void
+m_passiveh_null(int u, int v)
+{
+	lastu = u;
+	lastv = v;
+}
+
+void
+m_activeh_null(int u, int v)
+{
+	lastu = u;
+	lastv = v;
+}
