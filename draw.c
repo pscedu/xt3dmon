6938c7344825e991cb4b619291b6e9821f0548d2
rbudden <>
2005-06-17 16:55:32 +0000

    optimize by drawing quads instead of multiple polygons; lots of lighting code (dgb stuff at the momement)

--- draw.c
+++ draw.c
@@ -177,35 +177,97 @@ draw_filled_node(struct node *n, float w, float h, float d)
 	} else
 		a = 1.0;
 
-	glColor4f(r, g, b, a);
-	glBegin(GL_POLYGON);
-	/* Bottom */
-	glVertex3f(x, y, z);
-	glVertex3f(x, y, z+d);		/*  1 */
-	glVertex3f(x+w, y, z+d);	/*  2 */
-	glVertex3f(x+w, y, z);		/*  3 */
-	glVertex3f(x, y, z);		/*  4 */
-	/* Back */
-	glVertex3f(x, y+h, z);		/*  5 */
-	glVertex3f(x, y+h, z+d);	/*  6 */
-	glVertex3f(x, y, z+d);		/*  7 */
-	/* Right */
-	glVertex3f(x+w, y, z+d);	/*  8 */
-	glVertex3f(x+w, y+h, z+d);	/*  9 */
-	glVertex3f(x, y+h, z+d);	/* 10 */
+#if 0
+	if(st.st_opts & OP_DEBUG) {
+//		GLfloat mat_specular[] =	{0.3, 0.8, 0.8, 0.45};
+		GLfloat mat_specular[] =	{1.0, 1.0, 1.0, 1.0};
 
-	glVertex3f(x, y+h, z);		/* 11 */
+		/* 0 - 128 */
+		GLfloat mat_shininess[] =	{100.0};
+//		GLfloat white_light[] =		{1.0, 1.0, 1.0, 1.0};
+//		GLfloat spec_light[] =		{1.0, 1.0, 1.0, 1.0};
+		GLfloat white_light[] =		{r, g, b, 1.0};
+		GLfloat spec_light[] =		{r, g, b, 1.0};
+		GLfloat lmodel_ambient[] = 	{0.1, 0.1, 0.1, 0.0};
+
+		/* (x,y,z,w) if w != 0 positional instead of directional */
+		GLfloat light_pos[] =		{110.74, 9.40, 0.61, 1.0};
+		GLfloat spot_dir[] = 		{0.26, 0.0, 0.96};
+
+		/* Follow Pos */
+//		GLfloat light_pos[] =		{st.st_x, st.st_y, st.st_z, 1.0};
+//		GLfloat spot_dir[] = 		{st.st_lx, st.st_ly, st.st_lz};
+
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, spec_light);
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+//		glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+
+		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 15.0);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_dir);
+		glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 8.0);
+
+		glEnable(GL_NORMALIZE);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+	}
+#endif
+
+	glColor4f(r, g, b, a);
+
+	glBegin(GL_QUADS);
+
+	/* Back  */
+	glVertex3f(x, y, z);
+	glVertex3f(x, y+h, z);
+	glVertex3f(x+w, y+h, z);
+	glVertex3f(x+w, y, z);
+
+	/* Front */
+	glVertex3f(x, y, z+d);
+	glVertex3f(x, y+h, z+d);
+	glVertex3f(x+w, y+h, z+d);
+	glVertex3f(x+w, y, z+d);
+
+	/* Right */
+	glVertex3f(x+w, y, z);
+	glVertex3f(x+w, y, z+d);
+	glVertex3f(x+w, y+h, z+d);
+	glVertex3f(x+w, y+h, z);
 
 	/* Left */
-	glVertex3f(x+w, y+h, z);	/* 12 */
-	glVertex3f(x+w, y, z);		/* 13 */
-	glVertex3f(x+w, y+h, z);	/* 14 */
-	/* Front */
-	glVertex3f(x+w, y+h, z+d);	/* 15 */
+	glVertex3f(x, y, z);
+	glVertex3f(x, y, z+d);
+	glVertex3f(x, y+h, z+d);
+	glVertex3f(x, y+h, z);
+
+	/* Top */
+	glVertex3f(x, y+h, z);
+	glVertex3f(x, y+h, z+d);
+	glVertex3f(x+w, y+h, z+d);
+	glVertex3f(x+w, y+h, z);
+
+	/* Bottom */
+	glVertex3f(x, y, z);
+	glVertex3f(x, y, z+d);
+	glVertex3f(x+w, y, z+d);
+	glVertex3f(x+w, y, z);
+
 	glEnd();
 
+#if 0
 	if (st.st_opts & OP_BLEND)
 		glDisable(GL_BLEND);
+
+	if(st.st_opts & OP_DEBUG) {
+		glDisable(GL_LIGHTING);
+		glDisable(GL_LIGHT0);
+	}
+#endif
+
 }
 
 __inline void
@@ -231,6 +293,7 @@ draw_wireframe_node(struct node *n, float w, float h, float d)
 
 	glLineWidth(1.0);
 	glColor3f(0.0f, 0.0f, 0.0f);
+
 	glBegin(GL_LINE_STRIP);
 	/* Bottom */
 	glVertex3f(x, y, z);
@@ -312,8 +375,43 @@ draw_textured_node(struct node *n, float w, float h, float d)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
 
+#if 0
+	if(st.st_opts & OP_DEBUG) {
+		GLfloat mat_specular[] =	{0.3, 0.8, 0.8, 0.45};
+//		GLfloat mat_specular[] =	{1.0, 1.0, 1.0, 1.0};
+
+		/* 0 - 128 */
+		GLfloat mat_shininess[] =	{10.0};
+		GLfloat white_light[] =		{1.0, 1.0, 1.0, 1.0};
+		GLfloat spec_light[] =		{1.0, 1.0, 1.0, 1.0};
+		GLfloat lmodel_ambient[] = 	{0.6, 0.6, 0.6, 1.0};
+
+		/* (x,y,z,w) if w != 0 positional instead of directional */
+		GLfloat light_pos[] =		{1.0, 1.0, 1.0, 0.0};
+		GLfloat spot_dir[] = 		{-1.0, -1.0, 0.0};
+
+		glClearColor(0.0,0.0,0.0,0.0);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, spec_light);
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+//		glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+
+		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 45.0);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_dir);
+		glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 2.0);
+
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_NORMALIZE);
+	}
+#endif 
+
+	glBegin(GL_QUADS);
+
 	/* Back  */
-	glBegin(GL_POLYGON);
 	glVertex3f(x, y, z);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x, y+h, z);
@@ -322,10 +420,8 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(uh, uw);
 	glVertex3f(x+w, y, z);
 	glTexCoord2f(uh, 0.0);
-	glEnd();
 
 	/* Front */
-	glBegin(GL_POLYGON);
 	glVertex3f(x, y, z+d);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x, y+h, z+d);
@@ -334,10 +430,8 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(uh, uw);
 	glVertex3f(x+w, y, z+d);
 	glTexCoord2f(uh, 0.0);
-	glEnd();
 
 	/* Right */
-	glBegin(GL_POLYGON);
 	glVertex3f(x+w, y, z);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x+w, y, z+d);
@@ -346,10 +440,8 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(uh, ud);
 	glVertex3f(x+w, y+h, z);
 	glTexCoord2f(uh, 0.0);
-	glEnd();
 
 	/* Left */
-	glBegin(GL_POLYGON);
 	glVertex3f(x, y, z);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x, y, z+d);
@@ -358,10 +450,8 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(uh, ud);
 	glVertex3f(x, y+h, z);
 	glTexCoord2f(uh, 0.0);
-	glEnd();
 
 	/* Top */
-	glBegin(GL_POLYGON);
 	glVertex3f(x, y+h, z);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x, y+h, z+d);
@@ -370,10 +460,8 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(ud, uw);
 	glVertex3f(x+w, y+h, z);
 	glTexCoord2f(ud, 0.0);
-	glEnd();
 
 	/* Bottom */
-	glBegin(GL_POLYGON);
 	glVertex3f(x, y, z);
 	glTexCoord2f(0.0, 0.0);
 	glVertex3f(x, y, z+d);
@@ -382,6 +470,7 @@ draw_textured_node(struct node *n, float w, float h, float d)
 	glTexCoord2f(ud, uw);
 	glVertex3f(x+w, y, z);
 	glTexCoord2f(ud, 0.0);
+
 	glEnd();
 
 	if (st.st_opts & OP_BLEND)
@@ -394,6 +483,14 @@ draw_textured_node(struct node *n, float w, float h, float d)
 		glDisable(GL_POLYGON_OFFSET_FILL);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
+
+#if 0
+	if(st.st_opts & OP_DEBUG) {
+		glDisable(GL_LIGHTING);
+		glDisable(GL_LIGHT0);
+		glDisable(GL_NORMALIZE);
+	}
+#endif 
 
 	glDisable(GL_TEXTURE_2D);
 }
