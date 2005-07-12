3861dcc1003e61edd6beac6613ba63e8ba040bd7
rbudden <>
2005-07-12 20:35:26 +0000

    allow changin of both format parameters; cleanup

--- png.c
+++ png.c
@@ -156,24 +156,19 @@ load_png(char *file)
 }
 
 void
-load_texture(void *data, GLint fmt, int id)
+load_texture(void *data, GLint ifmt, GLenum fmt, int id)
 {
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_2D, id);
 
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
 
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	/* fmt is either GL_INTENSITY, GL_RGBA, ... */
-	glTexImage2D(GL_TEXTURE_2D, 0, fmt, width, height, 0, GL_RGBA,
+	glTexImage2D(GL_TEXTURE_2D, 0, ifmt, width, height, 0, fmt,
 	    GL_UNSIGNED_BYTE, data);
 
 	free(data);
