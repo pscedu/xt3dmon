0d68ede636d5996ce791fcc8522e36260239f816
rbudden <>
2005-07-25 17:50:20 +0000

    goto_node code for VM_WIREDONE

--- uinp.c
+++ uinp.c
@@ -19,7 +19,8 @@ uinpcb_ss(void)
 		screenshot(buf_get(&uinp.uinp_buf), capture_mode);
 }
 
-#define GOTO_DIST 2.5
+#define GOTO_DIST_PHYS 2.5
+#define GOTO_DIST_LOG  2.5
 
 void
 uinpcb_goto(void)
@@ -74,19 +75,28 @@ uinpcb_goto(void)
 			if(mod == 2 || mod == 3) {
 
 				/* Change z and look vector */
-				cv.v_z += NODEDEPTH + GOTO_DIST;
+				cv.v_z += NODEDEPTH + GOTO_DIST_PHYS;
 				st.st_lz = -1.0;
 
 			} else {
-				cv.v_z -= GOTO_DIST;
+				cv.v_z -= GOTO_DIST_PHYS;
 				st.st_lz = 1.0;
 			}
 
 			break;
 
-		/* TODO */
 		case VM_WIRED:
+			/* XXX - snap to grid? */
 		case VM_WIREDONE:
+
+			/* Set to the front where the label will be */
+			cv.v_x -= GOTO_DIST_LOG;
+			cv.v_y += 0.5*NODEHEIGHT;
+			cv.v_z += 0.5*NODEWIDTH;
+			st.st_ly = st.st_lz = 0.0;
+			st.st_lx = 1.0;
+			break;
+
 		default:
 			break;
 	}
