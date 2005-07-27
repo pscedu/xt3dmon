d715b538bdfcc43b319234c49e8a11c5489de0bd
yanovich <>
2005-07-27 15:30:45 +0000

    binary search to find job by id

--- job.c
+++ job.c
@@ -5,10 +5,18 @@
 struct job *
 job_findbyid(int id)
 {
-	size_t n;
-	
-	for (n = 0; n < job_list.ol_cur; n++)
-		if (job_list.ol_jobs[n]->j_id == id)
+	size_t n, lo, hi;
+
+	lo = 0;
+	hi = job_list.ol_cur;
+	while (lo <= hi) {
+		n = MID(lo, hi);
+		if (n < id)
+			lo = n + 1;
+		else if (n > id)
+			hi = n - 1;
+		else
 			return (job_list.ol_jobs[n]);
+	}
 	return (NULL);
 }
