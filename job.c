48c05d3c6604cedad838d81c9e7669abb2255c24
yanovich <>
2005-07-27 18:05:19 +0000

    use the correct value in comparision

--- job.c
+++ job.c
@@ -7,14 +7,16 @@ struct job *
 job_findbyid(int id)
 {
 	size_t n, lo, hi;
+	int tid;
 
 	lo = 0;
-	hi = job_list.ol_cur;
+	hi = MAX(job_list.ol_tcur, job_list.ol_cur);
 	while (lo <= hi) {
 		n = MID(lo, hi);
-		if (n < id)
+		tid = job_list.ol_jobs[n]->j_id;
+		if (tid < id)
 			lo = n + 1;
-		else if (n > id)
+		else if (tid > id)
 			hi = n - 1;
 		else
 			return (job_list.ol_jobs[n]);
