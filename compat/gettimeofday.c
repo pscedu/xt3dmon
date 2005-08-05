9b3106fc521bd73c887952f46fd37d3926097986
rbudden <>
2005-08-05 18:25:21 +0000

    what was i thinking before

--- compat/gettimeofday.c
+++ compat/gettimeofday.c
@@ -8,6 +8,6 @@ gettimeofday(struct timeval *tv, __unused void *null)
 {
 	long tc;
 	tc = GetTickCount();
-	tv->tv_sec = tc * 1e6;
-	tv->tv_usec = tc;
+	tv->tv_sec = (time_t)(tc * 1e6);
+	tv->tv_usec = tc - (tv->tv_sec / 1e6);
 }
