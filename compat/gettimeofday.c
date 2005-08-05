c49cd0d7e77bbdf448408de11d32801becf4e1f5
yanovich <>
2005-08-05 19:43:53 +0000

    proper conversions to microseconds

--- compat/gettimeofday.c
+++ compat/gettimeofday.c
@@ -7,9 +7,11 @@ int
 gettimeofday(struct timeval *tv, __unused void *null)
 {
 	long tc;
+
+	/* mili seconds  -> sec & micro seconds */
 	tc = GetTickCount();
 	tv->tv_sec = (int)(tc / 1e3);
-	tv->tv_usec = tc - tv->tv_sec * 1e3;
+	tv->tv_usec = tc * 1e3 - tv->tv_sec * 1e6;
 
 	return 0;
 }
