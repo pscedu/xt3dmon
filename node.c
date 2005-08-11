d9f29f6106bf0e6ee935165dff39ca5a1d28d02d
yanovich <>
2005-08-11 19:29:23 +0000

    revise algorithms for node neighborhoods; these were a bitch

--- node.c
+++ node.c
@@ -8,7 +8,7 @@ node_physpos(struct node *node, int *r, int *cb, int *cg, int *m,
 {
 	int pos;
 
-	pos = (node - &nodes[0][0][0][0][0]) / sizeof(*node);
+	pos = node - &nodes[0][0][0][0][0];
 
 	*r = pos / (NCABS * NCAGES * NMODS * NNODES);
 	pos %= NCABS * NCAGES * NMODS * NNODES;
@@ -27,28 +27,44 @@ node_neighbor(struct node *node, int amt, int dir)
 	int r, cb, cg, m, n;
 	struct node *rn;
 	struct ivec iv;
+	int rem, adj;
 
-	if (dir == DIR_LEFT ||
-	    dir == DIR_DOWN ||
-	    dir == DIR_BACK)
-		amt *= -1;
+	adj = 1;
+	switch (dir) {
+	case DIR_LEFT:
+		dir = DIR_RIGHT;
+		adj *= -1;
+		break;
+	case DIR_DOWN:
+		dir = DIR_UP;
+		adj *= -1;
+		break;
+	case DIR_BACK:
+		dir = DIR_FORWARD;
+		adj *= -1;
+		break;
+	}
 
-	switch (st.st_vmode) {
-	case VM_PHYSICAL:
-		node_physpos(node, &r, &cb, &cg, &m, &n);
-		switch (dir) {
-		case DIR_RIGHT:
-			m += amt;
-			cb += m / NMODS;
-			m %= NMODS;
-			cb %= NCABS;
-			break;
-		case DIR_UP:
-			if (amt % 2) {
+	node_physpos(node, &r, &cb, &cg, &m, &n);
+	iv = node->n_wiv;
+	for (rem = abs(amt); rem > 0; rem--) {
+		switch (st.st_vmode) {
+		case VM_PHYSICAL:
+			switch (dir) {
+			case DIR_RIGHT:
+				m += adj;
+				if (m < 0) {
+					cb--;
+					m += NMODS;
+					cb += NCABS;
+				} else if (m >= NMODS)
+					cb++;
+				m %= NMODS;
+				cb %= NCABS;
+				break;
+			case DIR_UP:
 				/*
-				 * Moving an odd number of nodes,
-				 * which means we move from our
-				 * position within a blade.
+				 * Move our position within a blade.
 				 *
 				 *	n | need
 				 *	=========
@@ -58,20 +74,35 @@ node_neighbor(struct node *node, int amt, int dir)
 				 *	1 | 2
 				 */
 				if (n % 2)
-					n--;
-				else
 					n++;
+				else
+					n--;
+				if (n < 0)
+					n += NNODES;
 				n %= NNODES;
-			}
-			cg += amt / 2;
-			cg %= NCAGES;
-			break;
-		case DIR_FORWARD:
-			if (amt % 2) {
+				if (adj == -1) {
+					/*
+					 * If we ended up in the top
+					 * portion when traveling
+					 * down, we wrapped.
+					 */
+					if (n >= NNODES / 2) {
+						cg--;
+						cg += NCAGES;
+					}
+				} else {
+					/*
+					 * If we ended up in the
+					 * bottom portion, we wrapped.
+					 */
+					if (n < NNODES / 2)
+						cg++;
+				}
+				cg %= NCAGES;
+				break;
+			case DIR_FORWARD:
 				/*
-				 * Moving an odd number of nodes,
-				 * which means we move from our
-				 * position within a blade.
+				 * Move our position within a blade.
 				 *
 				 *	n | need
 				 *	=========
@@ -84,32 +115,54 @@ node_neighbor(struct node *node, int amt, int dir)
 					n--;
 				else
 					n++;
+				if (n < 0)
+					n += NNODES;
+				n %= NNODES;
+				if (adj == -1) {
+					/*
+					 * If we ended up in the
+					 * front column when moving
+					 * back, we wrapped.
+					 */
+					if ((n & 1) ^ ((n & 2) >> 1)) {
+						r--;
+						r += NROWS;
+					}
+				} else {
+					/*
+					 * If we ended up in the
+					 * back column when moving
+					 * forward, we wrapped.
+					 */
+					if (((n & 1) ^ ((n & 2) >> 1)) == 0)
+						r++;
+				}
+				r %= NROWS;
+				break;
 			}
-			r += amt / 2;
-			r %= NROWS;
+			rn = &nodes[r][cb][cg][m][n];
+			break;
+		case VM_WIRED:
+		case VM_WIREDONE:
+			switch (dir) {
+			case DIR_RIGHT:
+				iv.iv_x += adj;
+				iv.iv_x %= WIDIM_WIDTH;
+				break;
+			case DIR_UP:
+				iv.iv_y += adj;
+				iv.iv_y %= WIDIM_HEIGHT;
+				break;
+			case DIR_FORWARD:
+				iv.iv_z += adj;
+				iv.iv_z %= WIDIM_DEPTH;
+				break;
+			}
+			rn = wimap[iv.iv_x][iv.iv_y][iv.iv_z];
 			break;
 		}
-		rn = &nodes[r][cb][cg][m][n];
-		break;
-	case VM_WIRED:
-	case VM_WIREDONE:
-		iv = node->n_wiv;
-		switch (dir) {
-		case DIR_RIGHT:
-			iv.iv_x += amt;
-			iv.iv_x %= WIDIM_WIDTH;
-			break;
-		case DIR_UP:
-			iv.iv_y += amt;
-			iv.iv_y %= WIDIM_HEIGHT;
-			break;
-		case DIR_FORWARD:
-			iv.iv_z += amt;
-			iv.iv_z %= WIDIM_DEPTH;
-			break;
-		}
-		rn = wimap[iv.iv_x][iv.iv_y][iv.iv_z];
-		break;
+		if (rn->n_flags & NF_HIDE)
+			rem++;
 	}
 	return (rn);
 }
