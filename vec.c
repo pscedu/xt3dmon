2ab789e2e54cc957f5ab61cc3c1615b6ee75d359
yanovich <>
2005-08-12 16:16:14 +0000

    add crossproduct function

--- vec.c
+++ vec.c
@@ -15,3 +15,11 @@ vec_normalize(struct fvec *fvp)
 	fvp->fv_y /= mag;
 	fvp->fv_z /= mag;
 }
+
+void
+vec_crossprod(struct fvec *cross, struct fvec *a, struct fvec *b)
+{
+	cross->fv_x = a->fv_y * b->fv_z - b->fv_y * a->fv_z;
+	cross->fv_y = b->fv_x * a->fv_z - a->fv_x * b->fv_z;
+	cross->fv_z = a->fv_x * b->fv_y - b->fv_x * a->fv_y;
+}
