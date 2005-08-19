f3285fe08828b9ffdc54ea0229179aec18530475
yanovich <>
2005-08-19 14:00:28 +0000

    matrix egg; add some comments/XXX

--- eggs.c
+++ eggs.c
@@ -1,19 +1,18 @@
 /* $Id$ */
 
-/* Easter Eggs, Inside Jokes, and the Like... */
-
 #include "compat.h"
 #include "mon.h"
 
 int eggs;
 
 /*
-** Borg mode Easter egg
-*/
+ * Borg mode Easter egg
+ */
 void
 egg_borg(void)
 {
 	static struct state ost;
+	int opts;
 
 	if (eggs & EGG_BORG) {
 		/* Save current state */
@@ -25,25 +24,44 @@ egg_borg(void)
 		st.st_opts &= (~OP_GROUND);
 
 		tween_push(TWF_LOOK | TWF_POS);
-		st.st_x = -89.0; 	st.st_lx = 1.0;
-		st.st_y = 25.0;		st.st_ly = 0.0;
-		st.st_z = 35.0;		st.st_lz = 0.0;
+		st.st_x = -89.0;  st.st_lx = 1.0;
+		st.st_y =  25.0;  st.st_ly = 0.0;
+		st.st_z =  35.0;  st.st_lz = 0.0;
 		tween_pop(TWF_LOOK | TWF_POS);
 
 		refresh_state(ost.st_opts);
 	} else {
-		int opts;
-
 		/* Restore state from before borg mode */
 		opts = st.st_opts;
 		st.st_opts = ost.st_opts;
 		st.st_vmode = ost.st_vmode;
 
 		tween_push(TWF_LOOK | TWF_POS);
-		st.st_x = ost.st_x;	st.st_lx = ost.st_lx;
-		st.st_y = ost.st_y; 	st.st_ly = ost.st_ly;
-		st.st_z = ost.st_z; 	st.st_lz = ost.st_lz;
+		st.st_x = ost.st_x;  st.st_lx = ost.st_lx;
+		st.st_y = ost.st_y;  st.st_ly = ost.st_ly;
+		st.st_z = ost.st_z;  st.st_lz = ost.st_lz;
 		tween_pop(TWF_LOOK | TWF_POS);
+
+		/* refresh_state(opts); */
 	}
+	/* XXX: RF_INIT ? */
 	st.st_rf |= RF_TEX | RF_CLUSTER | RF_SELNODE;
+}
+
+void
+egg_matrix(void)
+{
+	static struct state ost;
+	int opts;
+
+	if (eggs & EGG_MATRIX) {
+		st.st_opts |= OP_NLABELS;
+
+		refresh_state(ost.st_opts);
+	} else {
+		opts = st.st_opts;
+		st.st_opts = ost.st_opts;
+
+		refresh_state(opts);
+	}
 }
