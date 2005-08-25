56986b004caec2c2120e64567eb9e72214179be3
yanovich <>
2005-08-25 19:13:25 +0000

    move png_write here

--- png.c
+++ png.c
@@ -147,3 +147,63 @@ png_load(char *file, unsigned int *w, unsigned int *h)
 	fclose(fp);
 	return (data);
 }
+
+/* Save buffer as PNG. */
+void
+png_write(FILE *fp, unsigned char *buf, long w, long h)
+{
+	png_structp png;
+	png_bytepp rows;
+	png_infop info;
+	int i;
+
+	/* Setup PNG file structure. */
+	if ((png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL,
+	    NULL, NULL)) == NULL)
+		errx(1, "png_create_write_struct failed");
+
+	if ((info = png_create_info_struct(png)) == NULL) {
+		png_destroy_write_struct(&png, NULL);
+		errx(1, "png_create_info_struct failed");
+	}
+
+	if (setjmp(png_jmpbuf(png))) {
+		png_destroy_write_struct(&png, NULL);
+		err(1, "setjmp");
+	}
+
+	/* We want I/O without zlib compression (highest quality & fast). */
+	png_init_io(png, fp);
+//	png_set_compression_level(png, Z_NO_COMPRESSION);
+	png_set_compression_level(png, 4);
+	png_set_compression_mem_level(png, 8);
+//	png_set_compression_strategy(png, Z_DEFAULT_STRATEGY);
+
+	/* Set the header/info. */
+	png_set_IHDR(png, info, w, h, 8, PNG_COLOR_TYPE_RGB_ALPHA,
+	    PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
+	    PNG_FILTER_TYPE_DEFAULT);
+
+	png_write_info(png, info);
+
+	/* Pack single byte pixels. */
+	png_set_packing(png);
+
+	if ((rows = malloc(sizeof(png_bytep) * h)) == NULL)
+		err(1, "malloc");
+
+	/*
+	 * Parse into rows [RGBA (4 pix * width)]
+	 * Note: this takes into account vertical flip
+	 * use rows[h] then call invert() does the same.
+	 */
+	for (i = 0; i < h; i++)
+		rows[h - i - 1] = buf + (i * w * 4);
+
+	/* Actaully write the data. */
+	png_write_image(png, rows);
+	png_write_end(png, info);
+	png_destroy_write_struct(&png, &info);
+
+	free(rows);
+}
