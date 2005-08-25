a0578f4bf41277491c4df61efc99f364a5eca088
yanovich <>
2005-08-25 18:08:42 +0000

    initial checkin

---
+++ hl.c
@@ -0,0 +1,26 @@
+/* $Id$ */
+
+#include "compat.h"
+#include "mon.h"
+
+void
+hl_clearall(void)
+{
+	size_t j;
+
+	for (j = 0; j < job_list.ol_cur; j++)
+		job_list.ol_jobs[j]->j_fill.f_a = 0.0f;
+	for (j = 0; j < NJST; j++)
+		jstates[j].js_fill.f_a = 0.0f;
+}
+
+void
+hl_restoreall(void)
+{
+	size_t j;
+
+	for (j = 0; j < job_list.ol_cur; j++)
+		job_list.ol_jobs[j]->j_fill.f_a = 1.0f;
+	for (j = 0; j < NJST; j++)
+		jstates[j].js_fill.f_a = 1.0f;
+}
