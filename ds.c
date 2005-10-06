fb1a643b88204642d93e1b1e5c8bf32e125de385
yanovich <>
2005-10-06 18:53:22 +0000

    reorganize datasrc shit so that it actually works

--- ds.c
+++ ds.c
@@ -17,69 +17,68 @@
 #define RDS_HOST	"mugatu"
 #define RDS_PORT	80
 
+int ds_http(const char *);
+
 struct datasrc {
 	const char	*ds_lpath;
 	const char	*ds_rpath;
+	void		(*ds_parsef)(int *);
 	void		(*ds_dbf)(void);
 } datasrcs[] = {
-	{ _PATH_TEMPMAP,  _RPATH_TEMP,  db_tempmap },
-	{ _PATH_PHYSMAP,  _RPATH_PHYS,  db_physmap },
-	{ _PATH_JOBMAP,   _RPATH_JOBS,  db_jobmap },
-	{ _PATH_BADMAP,   _RPATH_BAD,   db_badmap },
-	{ _PATH_CHECKMAP, _RPATH_CHECK, db_checkmap },
-	{ _PATH_QSTAT,    _RPATH_QSTAT, db_qstat },
-	{ _PATH_STAT,     _PATH_STAT,   NULL },
-	{ _PATH_FAILMAP,  _RPATH_FAIL,  db_failmap }
+	{ _PATH_TEMPMAP,  _RPATH_TEMP,  parse_tempmap,	db_tempmap },
+	{ _PATH_PHYSMAP,  _RPATH_PHYS,  parse_physmap,	db_physmap },
+	{ _PATH_JOBMAP,   _RPATH_JOBS,  parse_jobmap,	db_jobmap },
+	{ _PATH_BADMAP,   _RPATH_BAD,   parse_badmap,	db_badmap },
+	{ _PATH_CHECKMAP, _RPATH_CHECK, parse_checkmap,	db_checkmap },
+	{ _PATH_QSTAT,    _RPATH_QSTAT, parse_qstat,	db_qstat },
+	{ _PATH_STAT,     _PATH_STAT,   parse_mem,	NULL },
+	{ _PATH_FAILMAP,  _RPATH_FAIL,  parse_failmap,	db_failmap }
 };
 #define NDATASRCS (sizeof(datasrcs) / sizeof(datasrcs[0]))
 
 void
 ds_refresh(int type, int flags)
 {
+	struct datasrc *ds;
+	int fd;
+
+	ds = &datasrcs[type];
 	switch (dsp) {
 	case DSP_LOCAL:
+		fd = open(ds->ds_lpath, O_RDONLY);
+		goto parse;
 	case DSP_REMOTE:
-		ds_open(type, flags);
+		fd = ds_http(ds->ds_rpath);
+parse:
+		if (fd == -1) {
+			if (flags & DSF_CRIT)
+				err(1, "datasrc open failed");
+			warn("datasrc (%d) open failed", type);
+			break;
+		}
+		ds->ds_parsef(&fd);
+		if (fd != -1)
+			close(fd);
 		break;
 	case DSP_DB:
+		ds->ds_dbf();
 		break;
 	}
 }
 
 int
-ds_open(int type, int flags)
+ds_http(const char *path)
 {
-	struct datasrc *ds;
-	int fd;
+	struct http_req r;
 
-	if (type < 0 || type >= (int)NDATASRCS)
-		errx(1, "datasrc type out of range");
-	ds = &datasrcs[type];
+	if (path == NULL)
+		errx(1, "no remote data available for datasrc type");
+	memset(&r, 0, sizeof(r));
+	r.htreq_server = RDS_HOST;
+	r.htreq_port = RDS_PORT;
 
-	switch (dsp) {
-	case DSP_REMOTE: {
-		struct http_req r;
-
-		if (ds->ds_rpath == NULL)
-			errx(1, "no remote data available for datasrc type");
-		memset(&r, 0, sizeof(r));
-		r.htreq_server = RDS_HOST;
-		r.htreq_port = RDS_PORT;
-
-		r.htreq_method = "GET";
-		r.htreq_version = "HTTP/1.1";
-		r.htreq_version = ds->ds_rpath;
-		fd = http_open(&r, NULL);
-		break;
-	    }
-	case DSP_LOCAL:
-		if ((fd = open(ds->ds_lpath, O_RDONLY)) == -1) {
-			if (flags & DSF_CRIT)
-				err(1, "%s", ds->ds_lpath);
-			warn("%s", ds->ds_lpath);
-			return (-1);
-		}
-		break;
-	}
-	return (fd);
+	r.htreq_method = "GET";
+	r.htreq_version = "HTTP/1.1";
+	r.htreq_url = path;
+	return (http_open(&r, NULL));
 }
