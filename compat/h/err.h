a4af246d8b459a9ea7ebb4c2c01db3e011939e94
yanovich <>
2005-10-31 21:49:12 +0000

    vwarnx

--- compat/h/err.h
+++ compat/h/err.h
@@ -1,4 +1,8 @@
+#include <stdarg.h>
+
+
 void	err(int, const char *, ...);
 void	errx(int, const char *, ...);
 void	warn(const char *, ...);
 void	warnx(const char *, ...);
+void	vwarnx(const char *, va_list);
