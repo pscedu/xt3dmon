f926d3c9fea5de03dc5dc433293aa006e788df6e
yanovich <>
2005-10-31 21:18:43 +0000

    vwarnx

--- compat/err.c
+++ compat/err.c
@@ -33,15 +33,20 @@ errx(int code, const char *fmt, ...)
 }
 
 void
+vwarnx(const char *fmt, va_list ap)
+{
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+}
+
+void
 warnx(const char *fmt, ...)
 {
 	va_list ap;
 
 	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
+	vwarnx(fmt, ap);
 	va_end(ap);
-
-	fprintf(stderr, "\n");
 }
 
 void
