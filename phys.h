f208758a46b70e08ef2b36ca0f8d19181e8db77d
yanovich <>
2005-12-02 18:03:16 +0000

    add parent container pointer; add flags for drawing skels

--- phys.h
+++ phys.h
@@ -14,11 +14,15 @@ struct physdim {
 	size_t			 pd_mag;
 	double			 pd_space;
 	struct physdim		*pd_contains;
+	struct physdim		*pd_containedby;
 	int			 pd_spans;
 	double			 pd_offset;
 	struct fvec		 pd_size;
+	int			 pd_flags;
 	LIST_ENTRY(physdim)	 pd_link;
 };
+
+#define PDF_SKEL (1<<0)
 
 LIST_HEAD(physdim_hd, physdim);
 
