b2dbd6aa21bd767192539769438a3670e73f30a2
yanovich <>
2005-12-19 19:30:11 +0000

    - reset linelen counter on newline encounter - replace whitespace with a newline if possible,   then fallback to inserting a gap after nonalnum

--- text.c
+++ text.c
@@ -8,19 +8,28 @@
 void
 text_wrap(char *s, size_t len, size_t maxlinelen)
 {
+	char *p, *t, *v;
 	size_t linelen;
-	char *p, *t;
 
 	linelen = 0;
 	for (p = s; *p != '\0'; p++) {
-		if (++linelen > maxlinelen) {
-			for (t = p; t > s && *t != '\n'; t--)
-				if (!isalnum(*t))
-					break;
+		if (*p == '\n')
+			linelen = 0;
+		else if (++linelen > maxlinelen) {
+			/* First attempt: try to break on whitespace. */
+			v = NULL;
+			for (t = p; t > s && *t != '\n'; t--) {
+				if (!isalnum(*t) && v == NULL)
+					v = t;
+				if (isspace(*t))
+					goto insertnl;
+			}
+			t = v;
 			if (t == s || *t++ == '\0')
 				t = p;
 			memmove(t + 1, t, len - 1 - (t - s));
 			s[len - 1] = '\0';
+insertnl:
 			*t = '\n';
 			linelen = p - t;
 		}
