1c0c305c2cb09caddbb7533f54844da492f3a573
yanovich <>
2005-12-19 08:12:36 +0000

    move images to img/ subdir; clean up data files; yod stuff; physconf stuff

--- pathnames.h
+++ pathnames.h
@@ -1,17 +1,15 @@
 /* $Id$ */
 
-#define _PATH_JOBMAP	"data/nids_list_phantom"
-#define _PATH_PHYSMAP	"data/rtrtrace"
-#define _PATH_FAILMAP	"data/fail"
-#define _PATH_TEMPMAP	"data/temps"
-#define _PATH_BADMAP	"/usr/users/torque/bad_nids_list_phantom"
-#define _PATH_CHECKMAP	"/usr/users/torque/to_check_nids_list_phantom"
-#define _PATH_QSTAT	"data/qstat.out"
+#define _PATH_NODE	"data/node"
+#define _PATH_JOB	"data/job"
+#define _PATH_YOD	"data/yod"
 
-#define _PATH_FLYBY	"data/flyby.data"
-#define _PATH_TEX	"data/texture%d.png"
-#define _PATH_BORG	"data/borg.png"
-#define _PATH_FONT	"data/font.png"
+#define _PATH_PHYSCONF	"data/phys.conf"
+
+#define _PATH_FLYBY	"img/flyby.data"
+#define _PATH_TEX	"img/texture%d.png"
+#define _PATH_BORG	"img/borg.png"
+#define _PATH_FONT	"img/font.png"
 #define _PATH_SSDIR	"snaps"
 #define _PATH_STAT	"/proc/self/stat"
 
