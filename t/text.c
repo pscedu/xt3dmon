3a76a872a5a002885d87a8dab33e0023c551b027
yanovich <>
2005-12-19 08:09:24 +0000

    spacing

--- t/text.c
+++ t/text.c
@@ -6,8 +6,8 @@
 int
 main(int argc, char *argv[])
 {
-	char buf[] = "some text that is totally awesome and one"
-	    "with the powers of the mighty nubus and dos command"
+	char buf[] = "some text that is totally awesome and one "
+	    "with the powers of the mighty nubus and dos command "
 	    "interpreters running off of video cards";
 
 	text_wrap(buf, sizeof(buf), 20);
