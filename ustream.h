b939a94f4a3a4304758d7d272a81322dd44a4e5d
yanovich <>
2006-01-06 20:25:51 +0000

    typos; make write() take const buf

--- ustream.h
+++ ustream.h
@@ -7,15 +7,15 @@ struct ustream {
 	int		 us_fd;
 	int		 us_type;
 	FILE		*us_fp;
-	ssize_t		 usi_lastread;
-	unsigned char	 usi_buf[BUFSIZ];
-	unsigned char	*usi_bufstart;
-	unsigned char	*usi_bufend;
+	ssize_t		 us_lastread;
+	unsigned char	 us_buf[BUFSIZ];
+	unsigned char	*us_bufstart;
+	unsigned char	*us_bufend;
 };
 
 struct ustream	*us_init(int, int, const char *);
 int		 us_close(struct ustream *);
-ssize_t		 us_write(struct ustream *, void *, size_t);
+ssize_t		 us_write(struct ustream *, const void *, size_t);
 char		*us_gets(char *, int, struct ustream *);
 int		 us_error(struct ustream *);
 int		 us_eof(struct ustream *);
