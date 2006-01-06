c66983c28eac5517a283ae3078237d860308962b
yanovich <>
2006-01-06 15:52:57 +0000

    initial checkin

---
+++ tools/update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# $Id$
+
+servroot=~/code/proj/xt3dmon
+webroot=/var/www/html/xtwmon
+sdb=sdb
+host=phantom.psc.edu
+
+# end config
+
+set -e
+
+ssh -gNL 3306:$sdb:3306 $host &
+pid=$(jobs -l | awk '{print $2}')
+
+cd $servroot/tools
+perl dumpdb.pl
+
+kill $pid
+
+cd $webroot/src
+perl xtwmon.pl
