707bdcbcce17521e07ee53bb4063e7e7b83abc7c
yanovich <>
2006-01-06 20:25:24 +0000

    vasprintf

--- compat/asprintf.c
+++ compat/asprintf.c
@@ -6,6 +6,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int
+vasprintf(char **ptr, char *fmt, ...)
+{
+	va_list ap;
+	int len;
+
+	va_start(ap, fmt);
+	len = vsnprintf(NULL, 0, fmt, ap);
+	va_end(ap);
+
+	if (len == -1)
+			return (-1);
+
+	len++;
+	if ((*ptr = malloc(len)) == NULL)
+		return (-1);
+
+	va_start(ap, fmt);
+	len = vsnprintf(*ptr, len, fmt, ap);
+	va_end(ap);
+
+	return (0);
+}
 
 int
 asprintf(char **ptr, const char *fmt, ...)
