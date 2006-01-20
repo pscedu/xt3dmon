c71199f5be0653d15d67b9496f4ff0818fd61a60
yanovich <>
2006-01-20 20:37:34 +0000

    negmodf and first stab at matrix_multiply

--- math.c
+++ math.c
@@ -1,5 +1,7 @@
 /* $Id$ */
 
+#include <math.h>
+
 #include "mon.h"
 
 __inline int
@@ -15,4 +17,39 @@ negmod(int a, int b)
 	} else
 		c = a % b;
 	return (c);
+}
+
+__inline double
+negmodf(double a, double b)
+{
+	double c;
+
+	if (a < 0) {
+		c = -a;
+		c = fmod(c, b);
+		if (c)
+			c = b - c;
+	} else
+		c = fmod(a, b);
+	return (c);
+}
+
+void
+matrix_multiply(double *r, double *a, size_t anc, size_t anr,
+   double *b, size_t bnc, size_t bnr)
+{
+	size_t i, j, k, l;
+	double val;
+
+	if (anr != bnc)
+		errx(1, "matrix_multiply: a_nrows != b_ncols");
+
+	for (i = 0; i < anc; i++)
+		for (j = 0; j < bnr; j++) {
+			val = 0;
+			for (k = 0; k < anr; k++)
+				for (l = 0; l < bnc; l++)
+					val += a[i * anr + k] * b[j * bnr + l];
+			r[i * anr + j] = val;
+		}
 }
