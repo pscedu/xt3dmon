64aea4111bc7191625369d0f4ebf269e53b368b1
yanovich <>
2006-01-30 21:02:57 +0000

    use new option api

--- eggs.c
+++ eggs.c
@@ -12,27 +12,24 @@ void
 egg_borg(void)
 {
 	static struct state ost;
-	int opts;
 
-	opts = st.st_opts;
 	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
-
 	if (eggs & EGG_BORG) {
 		/* Save current state. */
 		ost = st;
 
 		st.st_vmode = VM_WIREDONE;
 		st.st_mode = SM_BORG;
-		st.st_opts |= OP_TEX;
-		st.st_opts &= ~OP_GROUND;
+		opt_enable(OP_TEX);
+		opt_disable(OP_GROUND);
 
 		vec_set(&st.st_v, -89.0, 25.0, 35.0);
 		vec_set(&st.st_lv, 1.0, 0.0, 0.0);
 		vec_set(&st.st_uv, 0.0, 1.0, 0.0);
 	} else {
-		/* Restore state from before borg mode. */
-		st.st_opts = ost.st_opts;
+		opt_flip(st.st_opts ^ ost.st_opts);
 		st.st_vmode = ost.st_vmode;
+		st.st_mode = ost.st_mode;
 //		st = ost;
 
 		st.st_v = ost.st_v;
@@ -41,8 +38,7 @@ egg_borg(void)
 	}
 	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
 
-	st.st_rf |= RF_CLUSTER | RF_SELNODE | RF_SMODE;	/* XXX: RF_INIT ? */
-	refresh_state(opts);
+	st.st_rf |= RF_CLUSTER | RF_SELNODE | RF_SMODE | RF_GROUND;	/* XXX: RF_INIT ? */
 }
 
 void
@@ -50,17 +46,15 @@ egg_matrix(void)
 {
 	static struct fill ofill_bg;
 	static struct state ost;
-	int opts;
-
-	opts = st.st_opts;
+printf("hi\n");
 	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
-
 	if (eggs & EGG_MATRIX) {
 		ost = st;
 
+		st.st_vmode = VM_PHYSICAL;
 		st.st_mode = SM_MATRIX;
-		st.st_opts |= OP_NLABELS;
-		st.st_opts &= ~(OP_GROUND | OP_WIREFRAME);
+		opt_enable(OP_NLABELS);
+		opt_disable(OP_GROUND | OP_WIREFRAME);
 
 		vec_set(&st.st_v, 128.0, 6.25, 8.0);
 		vec_set(&st.st_lv, -1.0, 0.0, 0.0);
@@ -71,7 +65,7 @@ egg_matrix(void)
 		fill_bg.f_g = 0.2;
 		fill_bg.f_b = 0.0;
 	} else {
-		st.st_opts = ost.st_opts;
+		opt_flip(st.st_opts ^ ost.st_opts);
 		/* Restore original mode unless it was changed. */
 		st.st_mode = st.st_mode == SM_MATRIX ?
 		    ost.st_mode : st.st_mode;
@@ -84,6 +78,5 @@ egg_matrix(void)
 	}
 	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
 
-	st.st_rf |= RF_CLUSTER | RF_SELNODE | RF_SMODE;
-	refresh_state(opts);
+	st.st_rf |= RF_CLUSTER | RF_SELNODE | RF_SMODE | RF_GROUND;
 }
