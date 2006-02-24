b39abb0ca9faba18cf6b04b3341fd1c8211a6df7
yanovich <>
2006-02-24 21:51:35 +0000

    let this compile: proper decls

--- ustrdtab.c
+++ ustrdtab.c
@@ -1,7 +1,12 @@
 /* $Id$ */
 
-struct ustrdtab ustrdtabs[] = {
-	ustrdtab_file,	/* LOCAL */
-	ustrdtab_file,	/* REMOTE */
-	ustrdtab_ssl,	/* SSL */
+#include "ustream.h"
+
+extern struct ustrdtab ustrdtab_file;
+extern struct ustrdtab ustrdtab_ssl;
+
+struct ustrdtab *ustrdtabs[] = {
+	&ustrdtab_file,	/* LOCAL */
+	&ustrdtab_file,	/* REMOTE */
+	&ustrdtab_ssl	/* SSL */
 };
