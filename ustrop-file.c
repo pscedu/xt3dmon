b0cb47a90ffc4e5763b99a008bab8312ce889c59
yanovich <>
2006-02-24 21:40:31 +0000

    move to new abstracted ustream interface; inline some stuff

--- ustrop-file.c
+++ ustrop-file.c
@@ -1,14 +1,5 @@
 /* $Id$ */
 
-/*
- * This file is worthless.
- *
- * This universal stream interface was created so fgets()
- * can be used on winsocks.  Since UNIX provides the same
- * syscalls on all descriptors (not special ones for sockets),
- * nothing is really needed here.
- */
-
 #include <sys/types.h>
 
 #include <err.h>
@@ -19,8 +10,17 @@
 
 #include "ustream.h"
 
+struct ustrop ustrdtab_winsock[] = {
+	ustrop_file_init,
+	ustrop_file_close,
+	ustrop_file_write,
+	ustrop_file_gets,
+	ustrop_file_error,
+	ustrop_file_eof
+};
+
 struct ustream *
-us_init(int fd, int type, const char *modes)
+ustrop_file_init(int fd, int type, const char *modes)
 {
 	struct ustream *usp;
 
@@ -33,7 +33,7 @@ us_init(int fd, int type, const char *modes)
 
 	if ((usp->us_fp = fdopen(fd, modes)) == NULL)
 		err(1, "fdopen");
-	
+
 	/* Disable libc stream buffering to avoid blocking. */
 	if (type == UST_SOCK)
 		setbuf(usp->us_fp, NULL);
@@ -41,39 +41,35 @@ us_init(int fd, int type, const char *modes)
 	return (usp);
 }
 
-int
-us_close(struct ustream *usp)
+__inline int
+ustrop_file_close(struct ustream *usp)
 {
-	FILE *fp;
-
-	fp = usp->us_fp;
-	free(usp);			/* XXX: errno */
-	return (fclose(fp));
+	fclose(usp->us_fp);
 }
 
 /*
  * Yes, this shouldn't be here, but it's needed for sockets.
  */
-ssize_t
-us_write(struct ustream *usp, const void *buf, size_t siz)
+__inline ssize_t
+ustrop_file_write(struct ustream *usp, const void *buf, size_t siz)
 {
 	return (write(usp->us_fd, buf, siz));
 }
 
-char *
-us_gets(char *s, int siz, struct ustream *usp)
+__inline char *
+ustrop_file_gets(char *s, int siz, struct ustream *usp)
 {
 	return (fgets(s, siz, usp->us_fp));
 }
 
-int
-us_error(struct ustream *usp)
+__inline int
+ustrop_file_error(struct ustream *usp)
 {
 	return (ferror(usp->us_fp));
 }
 
-int
-us_eof(struct ustream *usp)
+__inline int
+ustrop_file_eof(struct ustream *usp)
 {
 	return (feof(usp->us_fp));
 }
