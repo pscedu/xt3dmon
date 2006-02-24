ffb8bad0fe255d7e407e65464af316f698a70802
yanovich <>
2006-02-24 21:57:54 +0000

    add ssl stuff

--- ustream.h
+++ ustream.h
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 
+#include <openssl/ssl.h>
+
 #define UST_LOCAL	0
 #define UST_REMOTE	1
 #define UST_SSL		2
@@ -26,13 +28,15 @@ struct ustream {
 	unsigned char	 us_buf[BUFSIZ];
 	unsigned char	*us_bufstart;
 	unsigned char	*us_bufend;
+	SSL_CTX		*us_ctx;
+	SSL		*us_ssl;
 };
 
-struct ustream	*us_init(int, int, const char *);
-int		 us_close(struct ustream *);
-ssize_t		 us_write(const struct ustream *, const void *, size_t);
-char		*us_gets(const struct ustream *, char *, int);
-int		 us_error(const struct ustream *);
-int		 us_eof(const struct ustream *);
+struct ustream		*us_init(int, int, const char *);
+int			 us_close(struct ustream *);
+ssize_t			 us_write(const struct ustream *, const void *, size_t);
+char			*us_gets(const struct ustream *, char *, int);
+int			 us_error(const struct ustream *);
+int			 us_eof(const struct ustream *);
 
-extern struct ustrdtab *ustrdtabs[NUST];
+extern struct ustrdtab	*ustrdtabs[NUST];
