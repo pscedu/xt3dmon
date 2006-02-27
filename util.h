0a7f3b06a37efd5f35b0aab649ef32dd6cb1a867
yanovich <>
2006-02-27 19:08:14 +0000

    move strnchr here

--- util.h
+++ util.h
@@ -1,4 +1,7 @@
 /* $Id$ */
 
-int	baseconv(int);
-void	base64_encode(const char *, char *);
+#include <sys/types.h>
+
+int	 baseconv(int);
+void	 base64_encode(const char *, char *);
+char	*strnchr(const char *, char, size_t);
