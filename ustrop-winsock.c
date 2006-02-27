16b5ad91a14090924727ce1063256c9c550ee8d8
yanovich <>
2006-02-27 19:11:52 +0000

    ustrop makeover

--- ustrop-winsock.c
+++ ustrop-winsock.c
@@ -10,73 +10,26 @@
 #include "cdefs.h"
 #include "ustream.h"
 
-struct ustrop ustrdtab_winsock[] = {
-	ustrop_winsock_init,
-	ustrop_winsock_close,
-	ustrop_winsock_write,
-	ustrop_winsock_gets,
-	ustrop_winsock_error,
-	ustrop_winsock_eof
-};
-
 struct ustream *
-us_winsock_init(int fd, int type, __unused const char *modes)
+ustrop_winsock_init(const struct ustream *usp)
 {
-	struct ustream *usp;
-
-	if ((usp = malloc(sizeof(*usp))) == NULL)
-		err(1, "malloc");
-	memset(usp, 0, sizeof(*usp));
-
-	usp->us_fd = fd;
-	usp->us_type = type;
-
-	if (type == UST_FILE)
-		if ((usp->us_fp = fdopen(fd, modes)) == NULL)
-			err(1, "fdopen");
-
 	return (usp);
 }
 
 __inline int
-us_close(struct ustream *usp)
+ustrop_winsock_close(const struct ustream *usp)
 {
 	return (closesocket(usp->us_fd));
 }
 
 ssize_t
-us_write(struct ustream *usp, const void *buf, size_t siz)
+ustrop_winsock_write(const struct ustream *usp, const void *buf, size_t siz)
 {
-	ssize_t written;
-
-	written = 0; /* gcc */
-	switch (usp->us_type) {
-	case UST_FILE:
-errx(1, "unimplemented");
-//		written = write(usp->us_fd, buf, siz);
-		break;
-	case UST_SOCK:
-		written = send(usp->us_fd, buf, siz, 0);
-		break;
-	}
-	return (written);
+	return (send(usp->us_fd, buf, siz, 0));
 }
 
 char *
-strnchr(char *s, char c, size_t len)
-{
-	size_t pos;
-
-	for (pos = 0; s[pos] != c && pos < len; pos++)
-		;
-
-	if (pos == len)
-		return (NULL);
-	return (s + pos);
-}
-
-char *
-us_gets(char *s, int siz, struct ustream *usp)
+ustrop_winsock_gets(char *s, int siz, struct ustream *usp)
 {
 	size_t total, chunksiz;
 	char *ret, *nl, *endp;
@@ -85,63 +38,64 @@ us_gets(char *s, int siz, struct ustream *usp)
 
 	remaining = siz - 1;		/* NUL termination. */
 	total = 0;
-	ret = NULL; /* gcc */
-	switch (usp->us_type) {
-	case UST_FILE:
-		ret = fgets(s, siz, usp->us_fp);
-		break;
-	case UST_SOCK:
-		ret = s;
-		while (remaining > 0) {
-			/* Look for newline in current buffer. */
-			if (usp->us_bufstart) {
-				if ((nl = strnchr(usp->us_bufstart, '\n',
-				    usp->us_bufend - usp->us_bufstart)) != NULL)
-					endp = nl;
-				else
-					endp = usp->us_bufend;
-				chunksiz = MIN(endp - usp->us_bufstart + 1, remaining);
+	ret = s;
+	while (remaining > 0) {
+		/* Look for newline in current buffer. */
+		if (usp->us_bufstart) {
+			if ((nl = strnchr(usp->us_bufstart, '\n',
+			    usp->us_bufend - usp->us_bufstart)) != NULL)
+				endp = nl;
+			else
+				endp = usp->us_bufend;
+			chunksiz = MIN(endp - usp->us_bufstart + 1, remaining);
 
-				/* Copy all data up to any newline. */
-				memcpy(s + total, usp->us_bufstart, chunksiz);
-				remaining -= chunksiz;
-				total += chunksiz;
-				usp->us_bufstart += chunksiz;
-				if (usp->us_bufstart > usp->us_bufend)
-					usp->us_bufstart = NULL;
-				if (nl)
-					break;
-			}
-
-			/* Not found, read more. */
-			chunksiz = MIN(remaining, sizeof(usp->us_buf));
-			nr = recv(usp->us_fd, usp->us_buf, chunksiz, 0);
-			usp->us_lastread = nr;
-
-			if (nr == -1 || nr == 0)
-				return (NULL);
-
-			usp->us_bufstart = usp->us_buf;
-			usp->us_bufend = usp->us_buf + nr - 1;
+			/* Copy all data up to any newline. */
+			memcpy(s + total, usp->us_bufstart, chunksiz);
+			remaining -= chunksiz;
+			total += chunksiz;
+			usp->us_bufstart += chunksiz;
+			if (usp->us_bufstart > usp->us_bufend)
+				usp->us_bufstart = NULL;
+			if (nl)
+				break;
 		}
-		/*
-		 * This should be safe because total is
-		 * bound by siz - 1.
-		 */
-		s[total] = '\0';
-		break;
+
+		/* Not found, read more. */
+		chunksiz = MIN(remaining, sizeof(usp->us_buf));
+		nr = recv(usp->us_fd, usp->us_buf, chunksiz, 0);
+		usp->us_lastread = nr;
+
+		if (nr == -1 || nr == 0)
+			return (NULL);
+
+		usp->us_bufstart = usp->us_buf;
+		usp->us_bufend = usp->us_buf + nr - 1;
 	}
+	/*
+	 * This should be safe because total is
+	 * bound by siz - 1.
+	 */
+	s[total] = '\0';
 	return (ret);
 }
 
 __inline int
-us_error(struct ustream *usp)
+ustrop_winsock_error(struct ustream *usp)
 {
 	return (usp->us_lastread == -1);
 }
 
 __inline int
-us_eof(struct ustream *usp)
+ustrop_winsock_eof(struct ustream *usp)
 {
 	return (usp->us_lastread == 0);
 }
+
+struct ustrop ustrdtab_winsock = {
+	ustrop_winsock_init,
+	ustrop_winsock_close,
+	ustrop_winsock_write,
+	ustrop_winsock_gets,
+	ustrop_winsock_error,
+	ustrop_winsock_eof
+};
