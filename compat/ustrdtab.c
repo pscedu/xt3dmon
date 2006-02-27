d32625c5318f58db82c786d3a1f3a8925f9e1be5
yanovich <>
2006-02-27 19:08:28 +0000

    let this compile; add socketclosef

--- compat/ustrdtab.c
+++ compat/ustrdtab.c
@@ -1,7 +1,15 @@
 /* $Id$ */
 
+#include "ustream.h"
+
+extern struct ustrdtab ustrdtab_file;
+extern struct ustrdtab ustrdtab_winsock;
+extern struct ustrdtab ustrdtab_ssl;
+
 struct ustrdtab ustrdtabs[] = {
 	ustrdtab_file,		/* LOCAL */
 	ustrdtab_winsock,	/* REMOTE */
 	ustrdtab_ssl,		/* SSL */
 };
+
+int (*socketclosef)(int) = close;
