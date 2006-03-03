3b20cb4e83cdbc43630be2287e0bc703895bcefd
yanovich <>
2006-03-03 19:24:09 +0000

    fix protos and decls; fix off-by-one when searching for EOL in gets()

--- ustrop-winsock.c
+++ ustrop-winsock.c
@@ -9,11 +9,12 @@
 
 #include "cdefs.h"
 #include "ustream.h"
+#include "util.h"
 
-struct ustream *
+int
 ustrop_winsock_init(const struct ustream *usp)
 {
-	return (usp);
+	return (1);
 }
 
 __inline int
@@ -29,7 +30,7 @@ ustrop_winsock_write(const struct ustream *usp, const void *buf, size_t siz)
 }
 
 char *
-ustrop_winsock_gets(char *s, int siz, struct ustream *usp)
+ustrop_winsock_gets(struct ustream *usp, char *s, int siz)
 {
 	size_t total, chunksiz;
 	char *ret, *nl, *endp;
@@ -43,7 +44,7 @@ ustrop_winsock_gets(char *s, int siz, struct ustream *usp)
 		/* Look for newline in current buffer. */
 		if (usp->us_bufstart) {
 			if ((nl = strnchr(usp->us_bufstart, '\n',
-			    usp->us_bufend - usp->us_bufstart)) != NULL)
+			    usp->us_bufend - usp->us_bufstart + 1)) != NULL)
 				endp = nl;
 			else
 				endp = usp->us_bufend;
@@ -80,18 +81,18 @@ ustrop_winsock_gets(char *s, int siz, struct ustream *usp)
 }
 
 __inline int
-ustrop_winsock_error(struct ustream *usp)
+ustrop_winsock_error(const struct ustream *usp)
 {
 	return (usp->us_lastread == -1);
 }
 
 __inline int
-ustrop_winsock_eof(struct ustream *usp)
+ustrop_winsock_eof(const struct ustream *usp)
 {
 	return (usp->us_lastread == 0);
 }
 
-struct ustrop ustrdtab_winsock = {
+struct ustrdtab ustrdtab_winsock = {
 	ustrop_winsock_init,
 	ustrop_winsock_close,
 	ustrop_winsock_write,
