b609d6a03fc6a0d7a1dd64450a62c92d3fc6151d
yanovich <>
2006-03-03 19:10:53 +0000

    return len, not zero

--- compat/asprintf.c
+++ compat/asprintf.c
@@ -28,7 +28,7 @@ vasprintf(char **ptr, const char *fmt, va_list ap)
 //			return (-1);
 	va_end(apdup);
 
-	return (0);
+	return (len);
 }
 
 int
@@ -41,13 +41,16 @@ asprintf(char **ptr, const char *fmt, ...)
 	len = vsnprintf(NULL, 0, fmt, ap);
 	va_end(ap);
 
+//	if (len == -1)
+//		return (-1);
+
 	len++;
 	if ((*ptr = malloc(len)) == NULL)
 		return (-1);
 
 	va_start(ap, fmt);
-	vsnprintf(*ptr, len, fmt, ap);
+	len = vsnprintf(*ptr, len, fmt, ap);
 	va_end(ap);
 
-	return (0);
+	return (len);
 }
