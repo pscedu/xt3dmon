f868267e128c6fe3cf080f27b98b09618eb20eb4
yanovich <>
2006-03-03 19:11:20 +0000

    kill socketclosef, replace with SOCKETCLOSE() in compat.h; fix decl

--- compat/ustrdtab.c
+++ compat/ustrdtab.c
@@ -6,10 +6,8 @@ extern struct ustrdtab ustrdtab_file;
 extern struct ustrdtab ustrdtab_winsock;
 extern struct ustrdtab ustrdtab_ssl;
 
-struct ustrdtab ustrdtabs[] = {
-	ustrdtab_file,		/* LOCAL */
-	ustrdtab_winsock,	/* REMOTE */
-	ustrdtab_ssl,		/* SSL */
+struct ustrdtab *ustrdtabs[] = {
+	&ustrdtab_file,		/* LOCAL */
+	&ustrdtab_winsock,	/* REMOTE */
+	&ustrdtab_ssl,		/* SSL */
 };
-
-int (*socketclosef)(int) = close;
