1e4570fce0fdd7bbda10f126ae04d12280e511c6
yanovich <>
2006-03-22 21:57:26 +0000

    mlock pass bufs

--- arch.c
+++ arch.c
@@ -1,6 +1,15 @@
 /* $Id$ */
 
+#include "mon.h"
+
+#include <sys/mman.h>
+
+#include "uinp.h"
+
 void
 arch_init(void)
 {
+	/* Don't want passwords to swap to disk... */
+	mlock(&login_auth, sizeof(login_auth));
+	mlock(&authbuf, sizeof(authbuf));
 }
