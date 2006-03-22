371153f877c2565f3d62572d74b0649b112e2ea6
yanovich <>
2006-03-22 17:38:14 +0000

    move CMP and SWAP here

--- util.h
+++ util.h
@@ -2,6 +2,16 @@
 
 #include <sys/types.h>
 
+#define CMP(a, b) \
+	((a) < (b) ? -1 : ((a) == (b) ? 0 : 1))
+
+#define SWAP(a, b, t)		\
+	do {			\
+		t = a;		\
+		a = b;		\
+		b = t;		\
+	} while (0)
+
 int	 baseconv(int);
 void	 base64_encode(const char *, char *);
 char	*strnchr(const char *, char, size_t);
