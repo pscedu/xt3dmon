aa39394d979f210c3981855d6abe6d2bfa2e0f64
yanovich <>
2006-03-22 19:13:03 +0000

    header cleanup; shuffle defns around to avoid prototypes; change tex_init so it auto-assigns texids and kill the fmt arg; selnode tex loading

--- tex.c
+++ tex.c
@@ -1,7 +1,37 @@
 /* $Id$ */
 
-#include "compat.h"
 #include "mon.h"
+
+#include <stdlib.h>
+
+#include "nodeclass.h"
+#include "pathnames.h"
+#include "env.h"
+
+/*
+ * "Initialize" texture data that are loaded into memory.
+ * ifmt is internal format: GL_INTENSITY, GL_RGBA, ...
+ */
+unsigned int
+tex_init(void *data, int ifmt, unsigned int w, unsigned int h)
+{
+	static unsigned int id;
+	int nd;
+
+	nd = GL_TEXTURE_2D;
+	glGenTextures(1, &id);
+	glBindTexture(nd, id);
+
+	glTexParameteri(nd, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(nd, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(nd, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(nd, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(nd, 0, ifmt, w, h, 0, GL_RGBA,
+	    GL_UNSIGNED_BYTE, data);
+	return (id++);
+}
 
 /*
  * Load all textures into memory.
@@ -9,61 +39,38 @@
 void
 tex_load(void)
 {
-	unsigned int i, w, h, texid;
+	unsigned int i, w, h;
 	char path[NAME_MAX];
 	void *data;
 
-	/* Read in texture IDs. */
-	for (i = 0, texid = 1; i < NSC; i++, texid++) {
-		snprintf(path, sizeof(path), _PATH_TEX, i);
+	/* Status class textures. */
+	for (i = 0; i < NSC; i++) {
+		snprintf(path, sizeof(path), _PATH_TEX_SC, i);
 		data = png_load(path, &w, &h);
-		tex_init(data, GL_RGBA, GL_RGBA, texid, w, h);
-		statusclass[i].nc_fill.f_texid[wid] = texid;
-
-		texid++;
-		tex_init(data, GL_INTENSITY, GL_RGBA, texid, w, h);
-		statusclass[i].nc_fill.f_texid_a[wid] = texid;
+		statusclass[i].nc_fill.f_texid[wid] = tex_init(data, GL_RGBA, w, h);
+		statusclass[i].nc_fill.f_texid_a[wid] = tex_init(data, GL_INTENSITY, w, h);
 		free(data);
 	}
 
-	/* Load the font texture -- background color over white in tex */
+	/* Load font texture -- always alpha-blended. */
 	data = png_load(_PATH_FONT, &w, &h);
-	tex_init(data, GL_INTENSITY, GL_RGBA, texid, w, h);
-	fill_font.f_texid_a[wid] = texid;
+	fill_font.f_texid_a[wid] = tex_init(data, GL_INTENSITY, w, h);
 	free(data);
 
-	texid++;
-	data = png_load(_PATH_BORG, &w, &h);
-	tex_init(data, GL_RGBA, GL_RGBA, texid, w, h);
-	fill_borg.f_texid[wid] = texid;
+	/* Borg texture. */
+	data = png_load(_PATH_TEX_BORG, &w, &h);
+	fill_borg.f_texid[wid] = tex_init(data, GL_RGBA, w, h);
+	fill_borg.f_texid_a[wid] = tex_init(data, GL_INTENSITY, w, h);
+	free(data);
 
-	texid++;
-	tex_init(data, GL_INTENSITY, GL_RGBA, texid, w, h);
-	fill_borg.f_texid_a[wid] = texid;
+	/* Selnode texture. */
+	data = png_load(_PATH_TEX_SN, &w, &h);
+	fill_selnode.f_texid[wid] = tex_init(data, GL_RGBA, w, h);
+	fill_selnode.f_texid_a[wid] = tex_init(data, GL_INTENSITY, w, h);
 	free(data);
 }
 
-/*
- * "Initialize" texture data that are loaded into memory.
- */
-void
-tex_init(void *data, GLint ifmt, GLenum fmt, GLuint id, GLuint w, GLuint h)
-{
-	glGenTextures(1, &id);
-	glBindTexture(GL_TEXTURE_2D, id);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	/* fmt is either GL_INTENSITY, GL_RGBA, ... */
-	glTexImage2D(GL_TEXTURE_2D, 0, ifmt, w, h, 0, fmt,
-	    GL_UNSIGNED_BYTE, data);
-}
-
-/* Delete textures from memory. */
+/* Remove textures from memory. */
 void
 tex_remove(void)
 {
