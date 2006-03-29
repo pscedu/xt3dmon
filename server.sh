fd75371104a419a59a72aaaae7ce6bfbd7dc5a21
yanovich <>
2006-03-29 21:38:53 +0000

    initial checkin

---
+++ server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# $Id$
+
+if pgrep -u yanovich xt3dmon >/dev/null 2>&1; then
+	echo "xt3dmon server already running"
+	exit 1
+fi
+
+XAUTHORITY=/var/gdm/:0.Xauth sudo -u yanovich echo ~/code/proj/xt3dmon/xt3dmon -display :0 -d &
