2ebce16a018fcbfb0987d5ad5ebdaccbce04da4c
yanovich <>
2006-03-31 17:25:20 +0000

    initial checkin

---
+++ service.c
@@ -0,0 +1,12 @@
+/* $Id$ */
+
+struct srv_node srv_nodes[] = {
+	{  151, "tg-login1.bigben.psc.teragrid.org", },
+	{    7, "tg-login2.bigben.psc.teragrid.org", },
+	{   11, "tg-login3.bigben.psc.teragrid.org", },
+	{  131, "tg-login4.bigben.psc.teragrid.org", },
+	{  640, "tg-login5.bigben.psc.teragrid.org", },
+	{  643, "tg-login6.bigben.psc.teragrid.org", },
+	{ 1408, "tg-gridftp1.bigben.psc.teragrid.org", },
+	{ 1411, "tg-gridftp2.bigben.psc.teragrid.org", }
+};
