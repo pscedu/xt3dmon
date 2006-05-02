988dfad17f78c28cb0c16ebdca5c9cf158556b21
yanovich <>
2006-05-02 15:59:50 +0000

    use better symbols for class range in class/legend labels

--- objlist.c
+++ objlist.c
@@ -57,11 +57,11 @@ struct nodeclass failclass[] = {
 	{ "6-10",		FILL_INIT(0.4f, 0.6f, 0.8f), 0 },
 	{ "11-15",		FILL_INIT(0.6f, 0.8f, 1.0f), 0 },
 	{ "15-20",		FILL_INIT(0.8f, 1.0f, 1.0f), 0 },
-	{ "20+",		FILL_INIT(1.0f, 1.0f, 1.0f), 0 }
+	{ ">20",		FILL_INIT(1.0f, 1.0f, 1.0f), 0 }
 };
 
 struct nodeclass rtclass[] = {
-	{ "0-10%",		FILL_INITA(1.0f, 1.0f, 0.4f, 0.5f), 0 },
+	{ "<10%",		FILL_INITA(1.0f, 1.0f, 0.4f, 0.5f), 0 },
 	{ "10-20%",		FILL_INITA(1.0f, 0.9f, 0.4f, 0.5f), 0 },
 	{ "20-30%",		FILL_INITA(1.0f, 0.8f, 0.4f, 0.5f), 0 },
 	{ "30-40%",		FILL_INITA(1.0f, 0.7f, 0.4f, 0.5f), 0 },
@@ -70,7 +70,7 @@ struct nodeclass rtclass[] = {
 	{ "60-70%",		FILL_INITA(1.0f, 0.4f, 0.4f, 0.7f), 0 },
 	{ "70-80%",		FILL_INITA(1.0f, 0.3f, 0.4f, 0.8f), 0 },
 	{ "80-90%",		FILL_INITA(1.0f, 0.2f, 0.4f, 0.9f), 0 },
-	{ "90-100%",		FILL_INITA(1.0f, 0.1f, 0.4f, 1.0f), 0 }
+	{ ">90%",		FILL_INITA(1.0f, 0.1f, 0.4f, 1.0f), 0 }
 };
 
 int
