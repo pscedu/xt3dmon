1001da44bc2737d9cbf34d2e06859701bd52c0f3
yanovich <>
2006-05-15 20:58:00 +0000

    use Bob's double circle idea; add lnseg stuff

--- deusex.c
+++ deusex.c
@@ -1,8 +1,13 @@
 /* $Id$ */
 
+#include "mon.h"
+
 #include <float.h>
 
+#include "env.h"
+#include "lnseg.h"
 #include "mark.h"
+#include "state.h"
 
 /*
 
@@ -25,35 +30,50 @@ dx_cue(double d)
 {
 	static double lastdiff = DBL_MAX;
 	static double t;
-	double diff;
+	double a, b, max, diff;
 	struct fvec sv, ev;
-	double a = 200;
-	double b = 100;
 	int ret;
 
-//	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
+	a = ROWWIDTH/4.0 - CABWIDTH;
+	b = CABHEIGHT/2.0;
+	max = MAX(a, b);
 
-	sv.fv_x = a * t;
-	sv.fv_z = (b * 3 / 4) * sin(t * M_PI * 8 / 9);
-	sv.fv_y = 0;
+	if (t > 4 * M_PI)
+		t = 0;
 
-	ev.fv_x = a * sin(t);
-	ev.fv_z = b * cos(t);
-	ev.fv_y = 0;
+	if (t < 2 * M_PI) {
+		sv.fv_x = a * sin(t - M_PI/2) + max;
+		sv.fv_y = b * cos(t - M_PI/2);
+		sv.fv_z = 0.0;
+	} else {
+		sv.fv_x = -a * sin(t - M_PI/2) - max;
+		sv.fv_y = b * cos(t - M_PI/2);
+		sv.fv_z = 0.0;
+	}
 
-	diff = DST(ev.fv_x, ev.fv_z, sv.fv_x, sv.fv_z);
+	ev.fv_x = 2 * sv.fv_x;
+	ev.fv_y = 2 * sv.fv_y;
+	ev.fv_z = 0.0;
+	vec_normalize(&ev);
 
-//	if (diff > lastdiff) {
-//		st.st_x = ev.fv_x;
-//		st.st_z = ev.fv_z;
-//	} else {
-//		st.st_x = sv.fv_x;
-//		st.st_z = sv.fv_z;
-//	}
-//	lastdiff = diff;
+	sv.fv_x += XCENTER;
+	sv.fv_y += YCENTER;
+	sv.fv_z += ZCENTER;
 
+	ev.fv_x += sv.fv_x;
+	ev.fv_y += sv.fv_y;
+	ev.fv_z += sv.fv_z;
+
+	lnseg_add(&sv, &ev);
 	mark_add(&sv);
-	mark_add(&ev);
+
+	t += 0.1;
+
+return 0;
+
+
+
+//	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
 
 //	st.st_lx = -st.st_x;
 //	st.st_lz = -st.st_z;
