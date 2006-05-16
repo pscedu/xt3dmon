e3b3c0f15cf3346445409980a169b552e35b1ed4
yanovich <>
2006-05-16 16:53:14 +0000

    initial cubanoid mode

--- deusex.c
+++ deusex.c
@@ -8,6 +8,7 @@
 #include "lnseg.h"
 #include "mark.h"
 #include "state.h"
+#include "tween.h"
 
 /*
 
@@ -28,65 +29,110 @@ dx_update(void)
 int
 dx_cue(double d)
 {
-	static double lastdiff = DBL_MAX;
 	static double t;
-	double a, b, max, diff;
-	struct fvec sv, ev;
+	static int dim;
+	double a, b, max;
+	struct fvec sv, uv, lv, xv;
 	int ret;
 
-	a = ROWWIDTH/4.0 - CABWIDTH;
-	b = CABHEIGHT/2.0;
-	max = MAX(a, b);
-
-	if (t > 4 * M_PI)
+	if (t > 4 * M_PI) {
 		t = 0;
+		dim++;
+
+		if (dim >= NDIM) {
+			ret = 1;
+			dim = 0;
+		}
+	}
+
+	switch (dim) {
+	case DIM_X:
+		a = CABHEIGHT/2.0;
+		b = ROWDEPTH/2.0;
+		break;
+	case DIM_Y:
+		a = ROWWIDTH/4.0 - CABWIDTH;
+		b = CABHEIGHT/2.0;
+		break;
+	case DIM_Z:
+		a = ROWWIDTH/4.0 - CABWIDTH;
+		b = CABHEIGHT/2.0;
+		break;
+	}
+	max = MAX(a, b);
 
 	if (t < 2 * M_PI) {
 		sv.fv_x = a * sin(t - M_PI/2) + max;
 		sv.fv_y = b * cos(t - M_PI/2);
-		sv.fv_z = 0.0;
+
+		uv.fv_x = sin(t - M_PI/2);
+		uv.fv_y = cos(t - M_PI/2);
 	} else {
 		sv.fv_x = -a * sin(t - M_PI/2) - max;
 		sv.fv_y = b * cos(t - M_PI/2);
-		sv.fv_z = 0.0;
+
+		uv.fv_x = -sin(t - M_PI/2);
+		uv.fv_y = cos(t - M_PI/2);
+	}
+	sv.fv_z = 0.0;
+	uv.fv_z = 0.0;
+
+	vec_set(&xv, 0.0, 0.0, 1.0);
+	vec_normalize(&uv);
+	vec_crossprod(&lv, &uv, &xv);
+
+	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
+
+	switch (dim) {
+	case DIM_X:
+		st.st_x = XCENTER;
+		st.st_y = YCENTER + sv.fv_x;
+		st.st_z = ZCENTER + sv.fv_y;
+
+		st.st_ux = 0.0;
+		st.st_uy = uv.fv_x;
+		st.st_uz = uv.fv_y;
+
+		st.st_lx = 0.0;
+		st.st_ly = lv.fv_x;
+		st.st_lz = lv.fv_y;
+		break;
+	case DIM_Y:
+		st.st_x = XCENTER + sv.fv_x;
+		st.st_y = YCENTER;
+		st.st_z = ZCENTER + sv.fv_y;
+
+		st.st_ux = uv.fv_x;
+		st.st_uy = 0.0;
+		st.st_uz = uv.fv_y;
+
+		st.st_lx = lv.fv_x;
+		st.st_ly = 0.0;
+		st.st_lz = lv.fv_y;
+		break;
+	case DIM_Z:
+		st.st_x = XCENTER + sv.fv_x;
+		st.st_y = YCENTER + sv.fv_y;
+		st.st_z = ZCENTER;
+
+		st.st_ux = uv.fv_x;
+		st.st_uy = uv.fv_y;
+		st.st_uz = 0.0;
+
+		st.st_lx = lv.fv_x;
+		st.st_ly = lv.fv_y;
+		st.st_lz = 0.0;
+		break;
 	}
 
-	ev.fv_x = 2 * sv.fv_x;
-	ev.fv_y = 2 * sv.fv_y;
-	ev.fv_z = 0.0;
-	vec_normalize(&ev);
+//st.st_lx = -st.st_x;
+//st.st_ly = -st.st_y;
+//st.st_lz = -st.st_z;
+//vec_normalize(&st.st_lv);
 
-	sv.fv_x += XCENTER;
-	sv.fv_y += YCENTER;
-	sv.fv_z += ZCENTER;
+	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
 
-	ev.fv_x += sv.fv_x;
-	ev.fv_y += sv.fv_y;
-	ev.fv_z += sv.fv_z;
+	t += 0.05;
 
-	lnseg_add(&sv, &ev);
-	mark_add(&sv);
-
-	t += 0.1;
-
-return 0;
-
-
-
-//	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
-
-//	st.st_lx = -st.st_x;
-//	st.st_lz = -st.st_z;
-//	st.st_ly = 0;
-
-//	vec_normalize(&st.st_lv);
-//	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
-
-	t += .01;
-	if (t >= 2 * M_PI) {
-		t = 0;
-		/* go into new dimension (x,y -> x,z) */
-		ret = 1;
-	}
 	return (ret);
 }
