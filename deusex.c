5b4f2d188dce4e88ea6b82cb0b03ed190f348d09
yanovich <>
2006-05-17 17:37:20 +0000

    add corkscrew; put quit check in right place for cubanoid

--- deusex.c
+++ deusex.c
@@ -19,13 +19,17 @@ int dx_orbit_v(void);
 int dx_cubanoid_x(void);
 int dx_cubanoid_y(void);
 int dx_cubanoid_z(void);
+int dx_corkscrew_x(void);
+
+int dx_corkscrew(int);
 
 int (*dxtab[])(void) = {
 	dx_orbit_u,
 	dx_orbit_v,
-	dx_cubanoid_x,
+//	dx_cubanoid_x,
 	dx_cubanoid_y,
-	dx_cubanoid_z
+//	dx_cubanoid_z,
+	dx_corkscrew_x
 };
 #define NENTRIES(t) (sizeof((t)) / sizeof((t)[0]))
 
@@ -96,10 +100,6 @@ dx_cubanoid(int dim)
 	int ret;
 
 	ret = 0;
-	if (t > 4 * M_PI) {
-		t = 0;
-		ret = 1;
-	}
 
 	switch (dim) {
 	case DIM_X:
@@ -197,7 +197,10 @@ dx_cubanoid(int dim)
 	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
 
 	t += 0.05;
-
+	if (t > 4 * M_PI) {
+		t = 0;
+		ret = 1;
+	}
 	return (ret);
 }
 
@@ -217,4 +220,100 @@ int
 dx_cubanoid_z(void)
 {
 	return (dx_cubanoid(DIM_Z));
+}
+
+int
+dx_corkscrew(int dim)
+{
+	static double t;
+	double a, b, c;
+	struct fvec sv;
+	int ret;
+
+	ret = 0;
+	switch (dim) {
+	case DIM_X:
+		a = CABHEIGHT / 4.0;
+		b = ((NROWS * (ROWDEPTH + NODESHIFT) +
+		    (NROWS - 1) * ROWSPACE)) / 4.0;
+		c = ROWWIDTH;
+		break;
+	case DIM_Y:
+		a = ROWWIDTH;
+		b = ROWDEPTH;
+		c = CABHEIGHT;
+		break;
+	case DIM_Z:
+		a = ROWWIDTH;
+		b = CABHEIGHT;
+		c = ROWDEPTH;
+		break;
+	}
+
+	sv.fv_x = t;
+	sv.fv_y = a * sin(t / c * 4.0 * M_PI);
+	sv.fv_z = b * cos(t / c * 4.0 * M_PI);
+
+	tween_push(TWF_LOOK | TWF_POS | TWF_UP);
+
+	switch (dim) {
+	case DIM_X:
+		st.st_x = t;
+		st.st_y = YCENTER + sv.fv_y;
+		st.st_z = ZCENTER + sv.fv_z;
+
+		if (t == 0.0) {
+			vec_set(&st.st_uv, 0.0, 1.0, 0.0);
+			vec_set(&st.st_lv, 1.0, 0.0, 0.0);
+		}
+		break;
+	case DIM_Y:
+		st.st_x = XCENTER + sv.fv_y;
+		st.st_y = t;
+		st.st_z = ZCENTER + sv.fv_z;
+
+		if (t == 0.0) {
+			vec_set(&st.st_uv, 0.0, 0.0, 1.0);
+			vec_set(&st.st_lv, 0.0, 1.0, 0.0);
+		}
+		break;
+	case DIM_Z:
+		st.st_x = XCENTER + sv.fv_y;
+		st.st_y = YCENTER + sv.fv_z;
+		st.st_z = t;
+
+		if (t == 0.0) {
+			vec_set(&st.st_uv, 0.0, 1.0, 0.0);
+			vec_set(&st.st_lv, 0.0, 0.0, 1.0);
+		}
+		break;
+	}
+
+	vec_rotate(&st.st_uv, &st.st_lv, 4.0 * M_PI / c);
+	tween_pop(TWF_LOOK | TWF_POS | TWF_UP);
+
+	t += 0.5;
+	if (t > c) {
+		t = 0;
+		ret = 1;
+	}
+	return (ret);
+}
+
+int
+dx_corkscrew_x(void)
+{
+	return (dx_corkscrew(DIM_X));
+}
+
+int
+dx_corkscrew_y(void)
+{
+	return (dx_corkscrew(DIM_Y));
+}
+
+int
+dx_corkscrew_z(void)
+{
+	return (dx_corkscrew(DIM_Z));
 }
