145b4f999fe13eba693d4b6c8fa8c4737722bd63
yanovich <>
2006-06-19 19:14:25 +0000

    initialize some vars to 0 to please gcc

--- deusex.c
+++ deusex.c
@@ -164,6 +164,7 @@ dxp_cuban8(int dim)
 	float mag;
 	int ret;
 
+	a = b = 0.0; /* gcc */
 	switch (dim) {
 	case DIM_X:
 		a = CABHEIGHT/4.0;
@@ -293,6 +294,7 @@ dxp_corkscrew(int dim)
 	int ret;
 
 	ret = 0;
+	a = b = c = 0.0; /* gcc */
 	switch (dim) {
 	case DIM_X:
 		a = CABHEIGHT / 4.0;
