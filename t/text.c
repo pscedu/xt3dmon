afc3c152d20af4ab4054cb5cd7f1d68d665fca48
yanovich <>
2006-07-20 22:19:21 +0000

    revise text_wrap: use buf structs and allow custom insert strings

--- t/text.c
+++ t/text.c
@@ -1,16 +1,29 @@
 /* $Id$ */
 
-#include <stdio.h>
 #include "mon.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "buf.h"
 
 int
 main(int argc, char *argv[])
 {
-	char buf[] = "some text that is totally awesome and one "
-	    "with the powers of the mighty nubus and dos command "
-	    "interpreters running off of video cards";
+	struct buf nbuf;
+	char buf[BUFSIZ] = "some text that is     totally awesome and one "
+	    "with the          powers ofthemighty nubus anddos command"
+	    "interpreters running off of video cards.  45,56,243,45,56,23";
 
-	text_wrap(buf, sizeof(buf), 20);
-	printf("%s\n", buf);
+	text_wrap(&nbuf, buf, 20, "<\n  ", 2);
+	printf("%s\n", buf_get(&nbuf));
+	buf_free(&nbuf);
+
+
+	buf_init(&nbuf);
+	buf_appendfv(&nbuf, "foo %d", 164);
+	buf_appendfv(&nbuf, "foo %d asdas ", 184);
+	buf_append(&nbuf, '\0');
+	printf("%s\n", buf_get(&nbuf));
 	exit(0);
 }
--- text.c
+++ text.c
@@ -5,33 +5,69 @@
 #include <ctype.h>
 #include <string.h>
 
+#include "cdefs.h"
+#include "buf.h"
+
 void
-text_wrap(char *s, size_t len, size_t maxlinelen)
+text_wrap(struct buf *bufp, const char *s, size_t maxlen, const char *ins, size_t mocklen)
 {
-	char *p, *t, *v;
-	size_t linelen;
+	const char *p, *t, *brk, *sp;
+	size_t linelen, inslen, i;
+
+	inslen = strlen(ins);
+
+	buf_init(bufp);
 
 	linelen = 0;
 	for (p = s; *p != '\0'; p++) {
-		if (*p == '\n')
+		if (*p == '\n') {
 			linelen = 0;
-		else if (++linelen > maxlinelen) {
+			buf_append(bufp, '\n');
+			while (isspace(*++p))
+				;
+			p--;
+		} else if (++linelen > maxlen) {
 			/* First attempt: try to break on whitespace. */
-			v = NULL;
-			for (t = p; t > s && *t != '\n'; t--) {
-				if (!isalnum(*t) && v == NULL)
-					v = t;
-				if (isspace(*t))
-					goto insertnl;
+			brk = NULL;
+			for (t = p - 1; p - t < linelen - mocklen && *t != '\n'; t--) {
+				if (!isalnum(*t) && brk == NULL)
+					brk = t + 1;
+				if (isspace(*t)) {
+					for (sp = t - 1; sp > s && isspace(*sp); sp--)
+						;
+					sp++;
+					for (i = 0; i < p - sp; i++)
+						buf_chop(bufp);
+					buf_appendv(bufp, ins);
+					linelen = mocklen;
+					p = t;
+					while (isspace(*++p))
+						;
+					p--;
+					goto next;
+				}
 			}
-			t = v;
-			if (t == s || *t++ == '\0')
+			/*
+			 * If no suitable break position was found,
+			 * force break at current end.
+			 */
+			if (brk == NULL)
 				t = p;
-			memmove(t + 1, t, len - 1 - (t - s));
-			s[len - 1] = '\0';
-insertnl:
-			*t = '\n';
-			linelen = p - t;
-		}
+			else
+				t = brk;
+
+			for (i = 0; i < p - t; i++)
+				buf_chop(bufp);
+insert:
+			buf_appendv(bufp, ins);
+			linelen = mocklen;
+			p = t - 1;
+			while (isspace(*p))
+				p++;
+		} else
+			buf_append(bufp, *p);
+next:
+		;
 	}
+	buf_append(bufp, '\0');
 }
