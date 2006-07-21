1b40658482af2ca1f2b0247225e33ce18fc81a07
yanovich <>
2006-07-21 15:38:17 +0000

    add strptime

--- compat.h
+++ compat.h
@@ -49,6 +49,8 @@ int vasprintf(char **, const char *, va_list);
 int snprintf(char *, size_t, const char *, ...);
 int vsnprintf(char *, size_t, const char *, va_list);
 
+char *strptime(const char *, const char *, struct tm *);
+
 int execvp(const char *, const char *const *);
 int gettimeofday(struct timeval *, void *);
 int ffs(int);
