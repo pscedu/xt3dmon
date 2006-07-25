1d0f045740797969f8d311bc184f96b31624a09c
yanovich <>
2006-07-25 14:44:35 +0000

    initial checkin

---
+++ rte.c
@@ -0,0 +1,29 @@
+/* $Id$ */
+
+#include "mon.h"
+
+#include <err.h>
+
+#include "env.h"
+#include "route.h"
+
+int
+rteport_to_dir(int port)
+{
+	switch (port) {
+	case RP_NEGX:
+		return (RD_NEGX);
+	case RP_POSX:
+		return (RD_POSX);
+	case RP_NEGY:
+		return (RD_NEGY);
+	case RP_POSY:
+		return (RD_POSY);
+	case RP_NEGZ:
+		return (RD_NEGZ);
+	case RP_POSZ:
+		return (RD_POSZ);
+	default:
+		err(1, "invalid rteport: %d", port);
+	}
+}
