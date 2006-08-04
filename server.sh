1fce871c7d809c320f59513d08d5ab79e5b576a2
yanovich <>
2006-08-04 17:01:40 +0000

    -N flag went away

--- server.sh
+++ server.sh
@@ -19,5 +19,6 @@ if ! [ -r $xauth ]; then
 fi
 
 cd /home/yanovich/code/proj/xt3dmon
-msg=$(XAUTHORITY=$xauth ./xt3dmon -display :0 -Nd 2>&1)
+msg=$(XAUTHORITY=$xauth ./xt3dmon -display :0 -d 2>&1)
 echo $msg | mail -s "xt3dmon down" yanovich@psc.edu
+echo $msg
