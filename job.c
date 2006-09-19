db760bf71bbe9ae01865e505d31545a494437f7a
yanovich <>
2006-09-19 20:38:24 +0000

    kill unused code

--- job.c
+++ job.c
@@ -40,40 +40,10 @@ job_findbyid(int id, int *pos)
 	return (NULL);
 }
 
-#if 0
-struct job *
-job_findbyid(size_t id)
-{
-	size_t n, tid;
-	int lo, hi;
-
-	lo = 0;
-	hi = MAX(job_list.ol_tcur, job_list.ol_cur);
-	if (hi == 0)
-		return (NULL);
-	while (lo <= hi) {
-		n = MID(lo, hi);
-		tid = job_list.ol_jobs[n]->j_id;
-		if (tid < id)
-			lo = n + 1;
-		else if (tid > id)
-			hi = n - 1;
-		else
-			return (job_list.ol_jobs[n]);
-	}
-	return (NULL);
-}
-#endif
-
 __inline void
 job_hl(struct job *j)
 {
 	j->j_fill.f_a = 1.0f;
-}
-
-void
-job_goto(__unused struct job *j)
-{
 }
 
 #if 0
