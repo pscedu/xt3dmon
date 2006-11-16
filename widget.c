28811f6850351af040144a64b4300208a19bffdc
yanovich <>
2006-11-16 18:37:56 +0000

    first primitives for OP_FANCY: nice lighting and smoother spheres

--- widget.c
+++ widget.c
@@ -67,13 +67,11 @@ draw_cube_filled(const struct fvec *dimp, int flags)
 	glVertex3d(x + w, y + h, z);
 	glVertex3d(x,     y + h, z);
 
-#if 0
 	/* Bottom */
 	glVertex3d(x,     y,     z);
 	glVertex3d(x,     y,     z + d);
 	glVertex3d(x + w, y,     z + d);
 	glVertex3d(x + w, y,     z);
-#endif
 }
 
 __inline void
@@ -170,6 +168,134 @@ draw_cube(const struct fvec *dimp, const struct fill *fp, int flags)
 			flags &= ~DF_FRAME;
 	}
 
+	if (st.st_opts & OP_FANCY) {
+		GLfloat ambientLight[]  = { 0.1f, 0.1f, 0.1f, 1.0f };
+		GLfloat diffuseLight[]  = { 0.2f, 0.2f, 0.2f, 1.0f };
+		GLfloat specularLight[] = { 0.4f, 0.4f, 0.4f, 1.0f };
+		GLfloat position0[] = {             -10.0f,             -10.0f,             -10.0f, 1.0f };
+		GLfloat position1[] = { dimp->fv_w + 10.0f,             -10.0f,             -10.0f, 1.0f };
+		GLfloat position2[] = {             -10.0f, dimp->fv_h + 10.0f,             -10.0f, 1.0f };
+		GLfloat position3[] = {             -10.0f,             -10.0f, dimp->fv_d + 10.0f, 1.0f };
+		GLfloat position4[] = { dimp->fv_w + 10.0f, dimp->fv_h + 10.0f,             -10.0f, 1.0f };
+		GLfloat position5[] = { dimp->fv_w + 10.0f,             -10.0f, dimp->fv_d + 10.0f, 1.0f };
+		GLfloat position6[] = {             -10.0f, dimp->fv_h + 10.0f, dimp->fv_d + 10.0f, 1.0f };
+		GLfloat position7[] = { dimp->fv_w + 10.0f, dimp->fv_h + 10.0f, dimp->fv_d + 10.0f, 1.0f };
+
+#define SQRT3 (0.577350269189626)
+		GLfloat look0[] = {  SQRT3,  SQRT3,  SQRT3, 1.0f };
+		GLfloat look1[] = { -SQRT3,  SQRT3,  SQRT3, 1.0f };
+		GLfloat look2[] = {  SQRT3, -SQRT3,  SQRT3, 1.0f };
+		GLfloat look3[] = {  SQRT3,  SQRT3, -SQRT3, 1.0f };
+		GLfloat look4[] = { -SQRT3, -SQRT3,  SQRT3, 1.0f };
+		GLfloat look5[] = { -SQRT3,  SQRT3, -SQRT3, 1.0f };
+		GLfloat look6[] = {  SQRT3, -SQRT3, -SQRT3, 1.0f };
+		GLfloat look7[] = { -SQRT3, -SQRT3, -SQRT3, 1.0f };
+
+		float materialLight[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+		{ struct fill f = *fp;
+
+		materialLight[0] = f.f_r * 0.8;
+		materialLight[1] = f.f_g * 0.8;
+		materialLight[2] = f.f_b * 0.8;
+
+//		fill_contrast(&f);
+		specularLight[0] = f.f_r * 1.1;
+		specularLight[1] = f.f_g * 1.1;
+		specularLight[2] = f.f_b * 1.1;
+		}
+
+		int param = GL_AMBIENT_AND_DIFFUSE;
+
+		if (fp->f_r > .8 || fp->f_g > .8 || fp->f_b > .8)
+			param = GL_SPECULAR;
+
+		glFrontFace(GL_CCW);
+		glColorMaterial(GL_FRONT_AND_BACK, param);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_COLOR_MATERIAL);
+//		glEnable(GL_LIGHT0);		/* ? - - */
+//		glEnable(GL_LIGHT1);		/* ? - - */
+//		glEnable(GL_LIGHT2);		/* ? - - */
+		glEnable(GL_LIGHT3);		/* + + + */
+//		glEnable(GL_LIGHT4);		/* ? - - */
+		glEnable(GL_LIGHT5);		/* - + + */
+		glEnable(GL_LIGHT6);		/* + - + */
+		glEnable(GL_LIGHT7);		/* - - + */
+		glEnable(GL_NORMALIZE);
+
+		glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, 1);
+
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, materialLight);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specularLight);
+
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT0, GL_POSITION, position0);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, look0);
+		glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT1, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT1, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT1, GL_POSITION, position1);
+		glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, look1);
+		glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT2, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT2, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT2, GL_POSITION, position2);
+		glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, look2);
+		glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT3, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT3, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT3, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT3, GL_POSITION, position3);
+		glLightfv(GL_LIGHT3, GL_SPOT_DIRECTION, look3);
+		glLightf(GL_LIGHT3, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT3, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT4, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT4, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT4, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT4, GL_POSITION, position4);
+		glLightfv(GL_LIGHT4, GL_SPOT_DIRECTION, look4);
+		glLightf(GL_LIGHT4, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT4, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT5, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT5, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT5, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT5, GL_POSITION, position5);
+		glLightfv(GL_LIGHT5, GL_SPOT_DIRECTION, look5);
+		glLightf(GL_LIGHT5, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT5, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT6, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT6, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT6, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT6, GL_POSITION, position6);
+		glLightfv(GL_LIGHT6, GL_SPOT_DIRECTION, look6);
+		glLightf(GL_LIGHT6, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT6, GL_LINEAR_ATTENUATION, 0.05);
+
+		glLightfv(GL_LIGHT7, GL_AMBIENT, ambientLight);
+		glLightfv(GL_LIGHT7, GL_DIFFUSE, diffuseLight);
+		glLightfv(GL_LIGHT7, GL_SPECULAR, specularLight);
+		glLightfv(GL_LIGHT7, GL_POSITION, position7);
+		glLightfv(GL_LIGHT7, GL_SPOT_DIRECTION, look7);
+		glLightf(GL_LIGHT7, GL_CONSTANT_ATTENUATION, 0.1);
+		glLightf(GL_LIGHT7, GL_LINEAR_ATTENUATION, 0.05);
+
+		glMateriali(GL_FRONT_AND_BACK, GL_SHININESS, 100);
+
+	}
+
 	if (fp->f_a != 1.0f) {
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, fp->f_blfunc ?
@@ -222,6 +348,19 @@ draw_cube(const struct fvec *dimp, const struct fill *fp, int flags)
 	if (fp->f_a != 1.0f)
 		glDisable(GL_BLEND);
 
+	if (st.st_opts & OP_FANCY) {
+		glDisable(GL_NORMALIZE);
+		glDisable(GL_LIGHT7);
+		glDisable(GL_LIGHT6);
+		glDisable(GL_LIGHT5);
+		glDisable(GL_LIGHT4);
+		glDisable(GL_LIGHT3);
+		glDisable(GL_LIGHT2);
+		glDisable(GL_LIGHT1);
+		glDisable(GL_LIGHT0);
+		glDisable(GL_COLOR_MATERIAL);
+		glDisable(GL_LIGHTING);
+	}
 	if (flags & DF_FRAME) {
 		/* Anti-aliasing */
 		glEnable(GL_LINE_SMOOTH);
@@ -242,9 +381,11 @@ draw_cube(const struct fvec *dimp, const struct fill *fp, int flags)
 		glDisable(GL_LINE_SMOOTH);
 		glDisable(GL_BLEND);
 	}
+
 }
 
 #define SPH_CUTS 7
+#define SPH_CUTS_FANCY 30
 
 /*
  * Draw a sphere at (x,y,z) with center at (x+r,y+r,z+r).
@@ -253,8 +394,13 @@ void
 draw_sphere(const struct fvec *dimp, const struct fill *fp, int flags)
 {
 	struct fill f_frame;
-	int ldflags;
+	int ldflags, cuts;
 	double r;
+
+	if (st.st_opts & OP_FANCY)
+		cuts = SPH_CUTS_FANCY;
+	else
+		cuts = SPH_CUTS;
 
 	ldflags = 0;
 	f_frame = *fp;
@@ -275,7 +421,7 @@ draw_sphere(const struct fvec *dimp, const struct fill *fp, int flags)
 
 		gluQuadricDrawStyle(quadric, GLU_FILL);
 		glColor4f(fp->f_r, fp->f_g, fp->f_b, fp->f_a);
-		gluSphere(quadric, r, SPH_CUTS, SPH_CUTS);
+		gluSphere(quadric, r, cuts, cuts);
 
 //		glDisable(GL_POLYGON_SMOOTH);
 
@@ -303,7 +449,7 @@ draw_sphere(const struct fvec *dimp, const struct fill *fp, int flags)
 		gluQuadricDrawStyle(quadric, GLU_SILHOUETTE);
 		glColor4f(f_frame.f_r, f_frame.f_g, f_frame.f_b,
 		    f_frame.f_a);
-		gluSphere(quadric, r, SPH_CUTS, SPH_CUTS);
+		gluSphere(quadric, r, cuts, cuts);
 
 		glDisable(GL_LINE_SMOOTH);
 		glDisable(GL_BLEND);
