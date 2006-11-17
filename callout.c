7b61e0a8186a4ae9e181be5ce64eae5ad87405b0
yanovich <>
2006-11-17 22:30:36 +0000

    wipe out dead code

--- callout.c
+++ callout.c
@@ -8,26 +8,10 @@
 #include "panel.h"
 #include "queue.h"
 #include "state.h"
-#include "tween.h"
 
 void
 cocb_fps(__unused int a)
 {
-#if 0
-	double fact;
-
-	if (fps && fps_cnt && fps != fps_cnt) {
-		fact = fps / (double)fps_cnt;
-		if (fact > 1.1)
-			fact = 1.1;
-		else if (fact < 0.9)
-			fact = 0.9;
-		tween_intv *= fact;
-		tween_thres *= fact;
-printf("adjusting by %.3f ->[%.3f %.3f]\n", fact, tween_intv, tween_thres);
-	}
-#endif
-
 	fps = fps_cnt;
 	fps_cnt = 0;
 	panel_rebuild(PANEL_FPS);
