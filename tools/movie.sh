b8a5d4ad03e1a17ee523021a7da27d905a1225e3
yanovich <>
2007-01-02 18:03:52 +0000

    add fps

--- tools/movie.sh
+++ tools/movie.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mencoder "mf://*.png" -o output.avi -ovc lavc -lavcopts vcodec=mpeg4:vqscale=1
+mencoder "mf://*.png" -o output.avi -mf fps=30 -ovc lavc -lavcopts vcodec=mpeg4:vqscale=1
