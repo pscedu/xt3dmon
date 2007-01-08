f41394af2071a36e6a0ce1f671ba2431effaa0e8
yanovich <>
2007-01-08 18:13:52 +0000

    fmt_scaled

--- util.h
+++ util.h
@@ -2,6 +2,9 @@
 
 #include <sys/types.h>
 
+/* 1000.00KB */
+#define FMT_SCALED_BUFSIZ	12
+
 #define CMP(a, b) \
 	((a) < (b) ? -1 : ((a) == (b) ? 0 : 1))
 
@@ -18,3 +21,4 @@ char		*strnchr(const char *, char, size_t);
 const char	*smart_dirname(const char *);
 const char	*smart_basename(const char *);
 void		 escape_printf(struct buf *, const char *);
+void		 fmt_scaled(size_t, char *);
