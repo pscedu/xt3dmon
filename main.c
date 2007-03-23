c39cd9342a281cb81a77cd1aff9b18743fd79e9f
yanovich <>
2007-03-23 21:07:38 +0000

    fix fuckup in last commit cause i cannot get simple things right

--- main.c
+++ main.c
@@ -934,7 +934,7 @@ glutInitWindowPosition(0, 0);
 	parse_colors(_PATH_COLORS);
 	parse_machconf(cfgfn);
 
-	nodes = NROWS * NCABS * NCAGES * NMODS * NNODES;
+	siz = NROWS * NCABS * NCAGES * NMODS * NNODES;
 	if ((nodes = calloc(siz, sizeof(*nodes))) == NULL)
 		err(1, "calloc");
 	if ((node_nidmap = calloc(machine.m_nidmax,
